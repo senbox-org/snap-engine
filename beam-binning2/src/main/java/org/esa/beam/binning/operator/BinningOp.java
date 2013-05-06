729dc56592e48c65ae3dbac1763f354bdcb5ece8
tom <tom.block@brockmann-consult.de>
2013-05-06 16:40:32 +0200

    split output creation and postprocessing

--- beam-binning2/src/main/java/org/esa/beam/binning/operator/BinningOp.java
+++ beam-binning2/src/main/java/org/esa/beam/binning/operator/BinningOp.java
@@ -22,12 +22,8 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
-import org.esa.beam.binning.BinningContext;
-import org.esa.beam.binning.SpatialBin;
-import org.esa.beam.binning.SpatialBinner;
-import org.esa.beam.binning.TemporalBin;
-import org.esa.beam.binning.TemporalBinSource;
-import org.esa.beam.binning.TemporalBinner;
+import org.esa.beam.binning.*;
+import org.esa.beam.binning.cellprocessor.CellProcessorChain;
 import org.esa.beam.framework.dataio.ProductIO;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.MetadataAttribute;
@@ -561,12 +557,17 @@ public class BinningOp extends Operator implements Output {
 
         long numberOfBins = spatialBinMap.size();
         final TemporalBinner temporalBinner = new TemporalBinner(binningContext);
+        final CellProcessorChain cellChain = new CellProcessorChain(binningContext);
         final TemporalBinList temporalBins = new TemporalBinList((int) spatialBinMap.size());
         Iterable<List<SpatialBin>> spatialBinListCollection = spatialBinMap.getBinCollection();
         for (List<SpatialBin> spatialBinList : spatialBinListCollection) {
             SpatialBin spatialBin = spatialBinList.get(0);
             long spatialBinIndex = spatialBin.getIndex();
-            final TemporalBin temporalBin = temporalBinner.processSpatialBins(spatialBinIndex, spatialBinList);
+            TemporalBin temporalBin = temporalBinner.processSpatialBins(spatialBinIndex, spatialBinList);
+            if (true) {
+                temporalBin = temporalBinner.computeOutput(spatialBinIndex, temporalBin);
+                temporalBin = cellChain.process(temporalBin);
+            }
             temporalBins.add(temporalBin);
         }
         stopWatch.stop();
