51484dd3f853b1dca603a6e571b47b6c2f830183
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-08-27 12:08:46 +0200

    New version

--- beam-python/src/main/resources/beampy/configtool.py
+++ beam-python/src/main/resources/beampy/configtool.py
@@ -6,81 +6,124 @@ import platform
 import argparse
 import zipfile
 import subprocess
+import logging
 
 
-def _configure_beampy(java_home=None, req_arch=None, force=False):
-    warnings = []
-    errors = []
+def _configure_beampy(java_home=None,
+                      req_arch=None,
+                      req_java=False,
+                      req_py=False,
+                      force=False):
 
     if req_arch:
         req_arch = req_arch.lower()
         act_arch = platform.machine().lower()
         if req_arch != act_arch:
-            warnings.append("architecture requirement not met: Python is " + act_arch + " but JVM requires " + req_arch)
+            logging.warning("architecture requirement possibly not met: Python is " + act_arch + " but JVM requires " + req_arch)
         is64 = sys.maxsize > 2 ** 31 - 1
         if is64 and not req_arch in ('amd64', 'ia64', 'x64', 'x86_64'):
-            warnings.append("architecture requirement not met: Python is 64 bit but JVM requires " + req_arch)
+            logging.warning("architecture requirement possibly not met: Python is 64 bit but JVM requires " + req_arch)
 
     beampy_dir = os.path.dirname(os.path.abspath(__file__))
 
     #
     # Extract a matching jpy binary distribution from ../lib/jpy.<platform>-<python-version>.zip
     #
+
     jpy_distr_name = 'jpy.' + sysconfig.get_platform() + '-' + sysconfig.get_python_version()
     jpy_info_file = os.path.join(beampy_dir, jpy_distr_name + '.info')
     if force or not os.path.exists(jpy_info_file):
         beampy_lib_dir = os.path.normpath(os.path.join(beampy_dir, '..', 'lib'))
         archive_path = os.path.join(beampy_lib_dir, jpy_distr_name + '.zip')
         if not os.path.exists(archive_path):
-            errors.append("can't find binary distribution '" + archive_path + "'")
-            return errors, warnings
+            logging.error("Can't find the required binary distribution '" + archive_path + "' of\n"
+                          "Python module 'jpy' for this system. You can try to generate one yourself.\n"
+                          "Please go to https://github.com/bcdev/jpy and follow the build instructions\n"
+                          "given there.")
+            raise IOError("can't find required binary distribution '" + archive_path + "'")
         #print('Found binary distribution ' + archive_path)
         #os.mkdir(os.path.join(basename)
+        logging.info("unzipping '" + archive_path + "'")
         with zipfile.ZipFile(archive_path) as zf:
             zf.extractall(path=beampy_dir)
 
+
     #
     # Execute jpyutil.py to write runtime configuration:
-    #   jpyconfig.properties - Configuration for Java about Python (jpy extension module)
+    # jpyconfig.properties - Configuration for Java about Python (jpy extension module)
     #   jpyconfig.py - Configuration for Python about Java (JVM)
     #
-    jpy_properties_file = os.path.join(beampy_dir, 'jpyconfig.properties')
-    if force or not os.path.exists(jpy_properties_file):
-        if not java_home:
-            jre_dir = os.path.join(beampy_dir,
-                                   '..',  # --> beam-python-<version>/
-                                   '..',  # --> modules/
-                                   '..',  # --> ${beam.home}
-                                   'jre') # --> ${beam.home}/jre
-            if os.path.exists(jre_dir):
-                java_home = os.path.normpath(jre_dir)
-        #print('java_home: ' + str(java_home))
-        jpyutil_file = os.path.join(beampy_dir, 'jpyutil.py')
-        jpyutil_cmd = [sys.executable, jpyutil_file, '-f']
-        if java_home:
-            jpyutil_cmd += ['--java_home', java_home]
-        retcode = subprocess.call(jpyutil_cmd)
-        if retcode:
-            errors.append('jpy configuration failed with return code ' + str(retcode))
 
-    return errors, warnings
+    retcode = 0
+
+    jpyutil_file = os.path.join(beampy_dir, 'jpyutil.py')
+    jpyconfig_java_file = os.path.join(beampy_dir, 'jpyconfig.properties')
+    jpyconfig_py_file = os.path.join(beampy_dir, 'jpyconfig.py')
+    if force or \
+            not os.path.exists(jpyconfig_java_file) or \
+            not os.path.exists(jpyconfig_py_file):
+        if os.path.exists(jpyutil_file):
+            #  Note 'jpyutil.py' has been unpacked by previous step, so we safely import it
+            import jpyutil
+
+            if not java_home:
+                jre_dir = os.path.join(beampy_dir,
+                                       '..',  # --> beam-python-<version>/
+                                       '..',  # --> modules/
+                                       '..',  # --> ${beam.home}
+                                       'jre')  # --> ${beam.home}/jre
+                if os.path.exists(jre_dir):
+                    java_home = os.path.normpath(jre_dir)
+
+            retcode = jpyutil.write_config_files(out_dir=beampy_dir,
+                                                 java_home_dir=java_home,
+                                                 req_java_api_conf=req_java,
+                                                 req_py_api_conf=req_py)
+        else:
+            raise RuntimeError("Can't find ")
+
+    return retcode
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Configures beampy, the BEAM Python interface.')
-    parser.add_argument('-a', '--arch',
+    parser.add_argument('--req_arch', default=None,
                         help='required JVM architecture, e.g. "amd64"')
-    parser.add_argument('-j', '--java_home',
+    parser.add_argument('--java_home', default=None,
                         help='Java JDK or JRE installation directory')
-    parser.add_argument('-f', '--force', action='store_true',
+    parser.add_argument("--log_file", action='store', default=None, help="Log file")
+    parser.add_argument("--log_level", action='store', default='INFO',
+                        help="Possible values: DEBUG, INFO, WARNING, ERROR")
+    parser.add_argument("-j", "--req_java", action='store_true', default=False,
+                        help="require that Java API configuration succeeds")
+    parser.add_argument("-p", "--req_py", action='store_true', default=False,
+                        help="require that Python API configuration succeeds")
+    parser.add_argument('-f', '--force', action='store_true', default=False,
                         help='force overwriting of output files')
     args = parser.parse_args()
-    errors, warnings = _configure_beampy(java_home=args.java_home, req_arch=args.arch, force=args.force)
-    with open('configtool.log', 'w') as f:
-        for error in errors:
-            f.write("error: " + error + "\n")
-        for warning in warnings:
-            f.write("warning: " + warning + "\n")
-    exit(0 if not len(errors) else 1)
 
+    log_level = getattr(logging, args.log_level.upper(), None)
+    if not isinstance(log_level, int):
+        raise ValueError('Invalid log level: %s' % log_level)
 
+    log_format = '%(levelname)s: %(message)s'
+    log_file = args.log_file
+    if log_file:
+        logging.basicConfig(format=log_format, level=log_level, filename=log_file, filemode='w')
+    else:
+        logging.basicConfig(format=log_format, level=log_level)
+
+    try:
+        retcode = _configure_beampy(java_home=args.java_home,
+                                    req_arch=args.req_arch,
+                                    req_java=args.req_java,
+                                    req_py=args.req_py,
+                                    force=args.force)
+    except Exception:
+        logging.exception("Configuration failed")
+        retcode = 10
+
+    exit(retcode)
+
+if __name__ == '__main__':
+    exit(_main())
