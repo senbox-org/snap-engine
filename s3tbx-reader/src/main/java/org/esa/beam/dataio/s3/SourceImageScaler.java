6e8a04f99d5f5d84b1a2cc210fb773310ad86d8a
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2014-08-29 15:36:29 +0200

    fix

--- s3tbx-reader/src/main/java/org/esa/beam/dataio/s3/SourceImageScaler.java
+++ s3tbx-reader/src/main/java/org/esa/beam/dataio/s3/SourceImageScaler.java
@@ -88,7 +88,7 @@ public class SourceImageScaler {
             RenderedImage renderedImage = image;
             final float xScale = scalings[0] * scaleRatio;
             final float yScale = scalings[1] * scaleRatio;
-            if (xScale != 1.0f && yScale != 1.0f) {
+            if (xScale != 1.0f || yScale != 1.0f) {
                 renderedImage = ScaleDescriptor.create(image, xScale, yScale, scaleTranslations[0], scaleTranslations[1],
                                                        interpolation, renderingHints);
             }
