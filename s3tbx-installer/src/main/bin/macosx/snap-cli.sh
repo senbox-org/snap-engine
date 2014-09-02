7f951621650858d9b344ed6783c30d723707848b
marpet <marco.peters@brockmann-consult.de>
2014-09-02 13:55:21 +0200

    visat --> snap

--- s3tbx-installer/src/main/bin/macosx/snap-cli.sh
+++ s3tbx-installer/src/main/bin/macosx/snap-cli.sh
@@ -1,23 +1,23 @@
 #! /bin/sh
 
-export S3TBX_HOME=${installer:sys.installationDir}
+export SNAP_HOME=${installer:sys.installationDir}
 
-if [ -z "$S3TBX_HOME" ]; then
+if [ -z "$SNAP_HOME" ]; then
     echo
-    echo Error: S3TBX_HOME not found in your environment.
-    echo Please set the S3TBX_HOME variable in your environment to match the
-    echo location of the S3TBX installation
+    echo Error: SNAP_HOME not found in your environment.
+    echo Please set the SNAP_HOME variable in your environment to match the
+    echo location of the SNAP installation
     echo
     exit 2
 fi
 
-export PATH=$PATH:$S3TBX_HOME/bin
+export PATH=$PATH:$SNAP_HOME/bin
 
 echo ""
-echo "Welcome to the S3TBX command-line interface!"
+echo "Welcome to the SNAP command-line interface!"
 echo "The following command-line tools are available:"
 echo "  gpt.sh            - General Graph Processing Tool"
 echo "  pconvert.sh       - General product conversion and quicklook generation"
-echo "  visat-d.sh        - VISAT application launcher for debugging"
+echo "  snap-d.sh         - SNAP application launcher for debugging"
 echo "Typing the name of the tool will output its usage information."
 echo ""
--- s3tbx-installer/src/main/bin/win/snap-cli.bat
+++ s3tbx-installer/src/main/bin/win/snap-cli.bat
@@ -1,16 +1,16 @@
 @echo off
 
-set S3TBX_HOME=${installer:sys.installationDir}
+set SNAP_HOME=${installer:sys.installationDir}
 
 echo.
-@echo Welcome to the S3TBX command-line interface!
+@echo Welcome to the SNAP command-line interface!
 @echo The following command-line tools are available:
 @echo   gpt.bat            - General Graph Processing Tool
 @echo   pconvert.bat       - General product conversion and quicklook generation
-@echo   visat-d.bat        - VISAT application launcher for debugging
+@echo   snap-d.bat         - SNAP application launcher for debugging
 @echo Typing the name of the tool will output its usage information.
 echo.
 
-cd "%S3TBX_HOME%\bin"
+cd "%SNAP_HOME%\bin"
 
 prompt $G$S
