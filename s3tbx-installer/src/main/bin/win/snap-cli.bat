7c166af6d012b172ff809b238a3b969c78dfb0ee
marpet <marco.peters@brockmann-consult.de>
2014-09-02 14:50:45 +0200

    no pconvert anymore

--- s3tbx-installer/src/main/bin/win/snap-cli.bat
+++ s3tbx-installer/src/main/bin/win/snap-cli.bat
@@ -6,7 +6,6 @@ echo.
 @echo Welcome to the SNAP command-line interface!
 @echo The following command-line tools are available:
 @echo   gpt.bat            - General Graph Processing Tool
-@echo   pconvert.bat       - General product conversion and quicklook generation
 @echo   snap-d.bat         - SNAP application launcher for debugging
 @echo Typing the name of the tool will output its usage information.
 echo.
