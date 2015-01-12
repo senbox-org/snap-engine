8bfe9161ed7846b762ac55c4b435982150e4c535
tom <tom.block@brockmann-consult.de>
2015-01-12 17:18:11 +0100

    writer - intermediate

--- beam-bigtiff/src/main/java/org/esa/beam/dataio/bigtiff/BigTiffProductWriter.java
+++ beam-bigtiff/src/main/java/org/esa/beam/dataio/bigtiff/BigTiffProductWriter.java
@@ -5,11 +5,14 @@ import org.esa.beam.framework.dataio.AbstractProductWriter;
 import org.esa.beam.framework.dataio.ProductWriterPlugIn;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.util.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
 
 public class BigTiffProductWriter extends AbstractProductWriter {
 
+    private File outputFile;
 
     public BigTiffProductWriter(ProductWriterPlugIn writerPlugIn) {
         super(writerPlugIn);
@@ -17,7 +20,19 @@ public class BigTiffProductWriter extends AbstractProductWriter {
 
     @Override
     protected void writeProductNodesImpl() throws IOException {
+        outputFile = null;
 
+        final File file;
+        if (getOutput() instanceof String) {
+            file = new File((String) getOutput());
+        } else {
+            file = (File) getOutput();
+        }
+
+        outputFile = FileUtils.ensureExtension(file, Constants.FILE_EXTENSIONS[0]);
+
+        deleteOutput();
+        updateProductName();
     }
 
     @Override
@@ -37,6 +52,16 @@ public class BigTiffProductWriter extends AbstractProductWriter {
 
     @Override
     public void deleteOutput() throws IOException {
+        if (outputFile != null && outputFile.isFile()) {
+            if (!outputFile.delete()) {
+                throw new IOException("Unable to delete file: " + outputFile.getAbsolutePath());
+            }
+        }
+    }
 
+    private void updateProductName() {
+        if (outputFile != null) {
+            getSourceProduct().setName(FileUtils.getFilenameWithoutExtension(outputFile));
+        }
     }
 }
--- beam-bigtiff/src/main/java/org/esa/beam/dataio/bigtiff/Constants.java
+++ beam-bigtiff/src/main/java/org/esa/beam/dataio/bigtiff/Constants.java
@@ -1,9 +1,10 @@
 package org.esa.beam.dataio.bigtiff;
 
-class Constants {
+public class Constants {
 
-    public static final String DESCRIPTION = "BigGeoTiff/GeoTiff data product.";
-    static final int PRIVATE_BEAM_TIFF_TAG_NUMBER = 65000;
+    public static final int PRIVATE_BEAM_TIFF_TAG_NUMBER = 65000;
+
+    static final String DESCRIPTION = "BigGeoTiff/GeoTiff data product.";
 
     static final String[] FORMAT_NAMES = new String[]{"BigGeoTiff"};
     static final String[] FILE_EXTENSIONS = new String[]{".tif", ".tiff"};
