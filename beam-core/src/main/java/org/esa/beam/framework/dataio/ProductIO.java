e96ffb9ea82bdde8220b32569470179956133607
lveci <lveci@array.ca>
2015-01-22 17:40:10 -0500

    merge

--- beam-core/src/main/java/org/esa/beam/framework/dataio/ProductIO.java
+++ beam-core/src/main/java/org/esa/beam/framework/dataio/ProductIO.java
@@ -429,6 +429,7 @@ public class ProductIO {
                     if (pm.isCanceled()) {
                         break;
                     }
+                    pm.setSubTaskName("Writing band '" + band.getName() + "'");
                     band.writeRasterDataFully(SubProgressMonitor.create(pm, 1));
                 }
             } finally {
