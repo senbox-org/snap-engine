885bfa364531a3fa5ddef141ad53ddcf46f8304c
marpet <marco.peters@brockmann-consult.de>
2015-02-06 09:36:19 +0100

    SNAP-67: moved aatsr-sst module s3tbx
    renamed helpset file

--- s3tbx-aatsr-sst-ui/src/main/java/org/esa/snap/aatsr/sst/docs/package-info.java
+++ s3tbx-aatsr-sst-ui/src/main/java/org/esa/snap/aatsr/sst/docs/package-info.java
@@ -1,4 +1,4 @@
-@HelpSetRegistration(helpSet = "sst.hs", position = 500)
+@HelpSetRegistration(helpSet = "help.hs", position = 500)
 package org.esa.snap.aatsr.sst.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
