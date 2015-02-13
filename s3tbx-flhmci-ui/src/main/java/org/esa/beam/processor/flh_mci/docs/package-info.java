a55d6a8e7e87e03166101b010cfacc7a64ed596a
marpet <marco.peters@brockmann-consult.de>
2015-02-13 10:20:14 +0100

    SNAP-67: changed position of flh_mci help

--- s3tbx-flhmci-ui/src/main/java/org/esa/beam/processor/flh_mci/docs/package-info.java
+++ s3tbx-flhmci-ui/src/main/java/org/esa/beam/processor/flh_mci/docs/package-info.java
@@ -1,4 +1,4 @@
-@HelpSetRegistration(helpSet = "help.hs", position = 600)
+@HelpSetRegistration(helpSet = "help.hs", position = 650)
 package org.esa.beam.processor.flh_mci.docs;
 
 import org.netbeans.api.javahelp.HelpSetRegistration;
