900f12796ebb6b525ee30a02b5fd6a24c8d99070
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2015-06-02 18:28:02 +0200

    use kind-of-lut

--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/synergy/SynL1COlciSceneRasterTransform.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/synergy/SynL1COlciSceneRasterTransform.java
@@ -7,6 +7,9 @@ import org.opengis.referencing.operation.MathTransform2D;
 import org.opengis.referencing.operation.NoninvertibleTransformException;
 import org.opengis.referencing.operation.TransformException;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * @author Tonio Fincke
  */
@@ -61,7 +64,7 @@ class SynL1COlciSceneRasterTransform implements SceneRasterTransform {
 
         @Override
         public MathTransform2D inverse() throws NoninvertibleTransformException {
-            return new SynL1COlciInverseMathTransform2D(columnMisregistrationBand, rowMisregistrationBand);
+            return inverse;
         }
 
         @Override
@@ -77,13 +80,62 @@ class SynL1COlciSceneRasterTransform implements SceneRasterTransform {
 
     private class SynL1COlciInverseMathTransform2D extends AbstractMathTransform implements MathTransform2D {
 
-        private final Band columnMisregistrationBand;
-        private final Band rowMisregistrationBand;
         private static final int dims = 2;
+        private final int[] columnMisregistration;
+        private final int[] rowMisregistration;
+
+        private final static int invalid_value = Integer.MIN_VALUE;
 
         SynL1COlciInverseMathTransform2D(Band columnMisregistrationBand, Band rowMisregistrationBand) {
-            this.columnMisregistrationBand = columnMisregistrationBand;
-            this.rowMisregistrationBand = rowMisregistrationBand;
+            columnMisregistration = createMisRegistrationArray(columnMisregistrationBand);
+            rowMisregistration = createMisRegistrationArray(rowMisregistrationBand);
+        }
+
+        private int[] createMisRegistrationArray(Band misregistrationBand) {
+            int[] misregistrationArray = new int[misregistrationBand.getSceneRasterWidth()];
+            Arrays.fill(misregistrationArray, invalid_value);
+            int[] forwardRegistration = new int[misregistrationArray.length];
+            try {
+                misregistrationBand.readPixels(0, 0, misregistrationBand.getSceneRasterWidth(),
+                                               misregistrationBand.getSceneRasterHeight(), forwardRegistration);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < misregistrationArray.length; i++) {
+                final int columnPosition = i + forwardRegistration[i];
+                if (columnPosition > 0 && columnPosition < misregistrationArray.length) {
+                    misregistrationArray[columnPosition] = -1 * forwardRegistration[i];
+                }
+            }
+            int lastValidValue = invalid_value;
+            for (int i = 0; i < misregistrationArray.length; i++) {
+                if (misregistrationArray[i] == invalid_value) {
+                    if (i < misregistrationArray.length - 1) {
+                        int nextValidValue = invalid_value;
+                        int nextValidIndex = invalid_value;
+                        for (int j = i; j < misregistrationArray.length; j++) {
+                            if (misregistrationArray[j] != invalid_value) {
+                                nextValidValue = misregistrationArray[j];
+                                nextValidIndex = j;
+                                break;
+                            }
+                        }
+                        if (lastValidValue == Integer.MIN_VALUE) {
+                            lastValidValue = nextValidValue;
+                        }
+                        if (nextValidIndex == invalid_value) {
+                            nextValidIndex = misregistrationArray.length - 1;
+                            nextValidValue = lastValidValue;
+                        }
+                        for (int j = nextValidIndex; j >= i; j--) {
+                            misregistrationArray[j] = (int)(lastValidValue +
+                                    (lastValidValue - nextValidValue) *
+                                            ((nextValidIndex - j) / (double)nextValidIndex - i));
+                        }
+                    }
+                }
+            }
+            return misregistrationArray;
         }
 
         @Override
@@ -95,18 +147,14 @@ class SynL1COlciSceneRasterTransform implements SceneRasterTransform {
                 final int secondIndex = firstIndex + 1;
                 final double srcPtX = srcPts[srcOff + firstIndex];
                 final double srcPtY = srcPts[srcOff + secondIndex];
-                final int columnMisregistration =
-                        columnMisregistrationBand.getSampleInt((int) srcPtX, 0);
-                final int rowMisregistration =
-                        rowMisregistrationBand.getSampleInt((int) srcPtX, 0);
-                dstPts[dstOff + firstIndex] = srcPtX - columnMisregistration;
-                dstPts[dstOff + secondIndex] = srcPtY - rowMisregistration;
+                dstPts[dstOff + firstIndex] = srcPtX + columnMisregistration[(int) srcPtX];
+                dstPts[dstOff + secondIndex] = srcPtY + rowMisregistration[(int) srcPtX];
             }
         }
 
         @Override
         public MathTransform2D inverse() throws NoninvertibleTransformException {
-            return new SynL1COlciForwardMathTransform2D(columnMisregistrationBand, rowMisregistrationBand);
+            return forward;
         }
 
         @Override
