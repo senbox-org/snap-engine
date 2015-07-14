4b2868de6c9f9171d65c600e9d9086e94d924e15
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-14 15:09:29 +0200

    Fixed SNAP-121, SNAP-122

--- s3tbx-kit/src/main/java/org/esa/s3tbx/about/S3tbxAboutBox.java
+++ s3tbx-kit/src/main/java/org/esa/s3tbx/about/S3tbxAboutBox.java
@@ -5,16 +5,30 @@
  */
 package org.esa.s3tbx.about;
 
-import javax.swing.JLabel;
 import org.esa.snap.rcp.about.AboutBox;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.Modules;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
 
 /**
  * @author Norman
  */
 @AboutBox(displayName = "S3TBX", position = 30)
-public class S3tbxAboutBox extends JLabel {
-    
+public class S3tbxAboutBox extends JPanel {
+
     public S3tbxAboutBox() {
-        super("<html>This is the fantastic <b>Sentinel-3 Toolbox</b>");
+        super(new BorderLayout(4, 4));
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+        ModuleInfo moduleInfo = Modules.getDefault().ownerOf(S3tbxAboutBox.class);
+        ImageIcon aboutImage = new ImageIcon(S3tbxAboutBox.class.getResource("about_s3tbx.jpg"));
+        JLabel iconLabel = new JLabel(aboutImage);
+        add(iconLabel, BorderLayout.CENTER);
+        add(new JLabel("<html><b>Sentinel-3 Toolbox (S3TBX) version " + moduleInfo.getImplementationVersion() + "</b>", SwingConstants.RIGHT), BorderLayout.SOUTH);
     }
 }
