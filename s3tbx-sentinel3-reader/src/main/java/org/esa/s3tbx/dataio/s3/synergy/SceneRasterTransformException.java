5e2c8b70b78d49f49274f7944a541f79b7e760c7
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2015-10-02 18:51:05 +0200

    moved scenerastertransform to s3tbx for syn l1c support

---
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/synergy/SceneRasterTransformException.java
@@ -0,0 +1,16 @@
+package org.esa.s3tbx.dataio.s3.synergy;
+
+/**
+ * @author Tonio Fincke
+ */
+public class SceneRasterTransformException extends Exception {
+
+    public SceneRasterTransformException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SceneRasterTransformException(String message) {
+        super(message);
+    }
+
+}
