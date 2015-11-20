a4f141b13bc254c87eb5f80b06248aeef5622459
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-11-20 12:54:21 +0100

    Updated API docs

---
+++ s3tbx-flhmci/src/main/java/org/esa/s3tbx/processor/flh_mci/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package provides an implementation for a Fluorescence Line Height (FLH)
+ * and Maximum Chlorophyl Index (MCI) Processor for ENVISAT MERIS products.
+ */
+package org.esa.s3tbx.processor.flh_mci;
