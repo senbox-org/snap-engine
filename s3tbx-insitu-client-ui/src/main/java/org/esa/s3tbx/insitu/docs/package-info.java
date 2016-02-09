d85ea1935d560f4ac057df04d701b7b211adc2c5
marpet <marco.peters@brockmann-consult.de>
2016-02-09 17:01:47 +0100

    bring help pages to life

---
+++ s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/docs/package-info.java
@@ -0,0 +1,4 @@
+@HelpSetRegistration(helpSet = "help.hs", position = 4653)
+package org.esa.s3tbx.insitu.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
--- s3tbx-insitu-client-ui/src/main/resources/org/esa/s3tbx/insitu/docs/help.hs
+++ s3tbx-insitu-client-ui/src/main/resources/org/esa/s3tbx/insitu/docs/help.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>FlhMci Processor Help</title>
+    <title>In-Situ Data Access Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.jhm"/>
