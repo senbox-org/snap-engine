9784103b1af62126afd5af09f3bb65f327dc2410
marpet <marco.peters@brockmann-consult.de>
2016-02-10 11:19:16 +0100

    updated ui

--- s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientForm.java
+++ s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientForm.java
@@ -2,10 +2,9 @@ package org.esa.s3tbx.insitu.ui;
 
 import com.bc.ceres.swing.TableLayout;
 import org.esa.snap.rcp.util.DateTimePicker;
-import org.jdesktop.swingx.HorizontalLayout;
-import org.jdesktop.swingx.VerticalLayout;
 
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -29,23 +28,6 @@ public class InsituClientForm extends JPanel {
     }
 
     private void initForm() {
-        setLayout(new VerticalLayout(4));
-
-        add(createDatabaseSelectionPanel());
-        add(createRequestConfigurationPanel());
-    }
-
-    private JPanel createRequestConfigurationPanel() {
-        final JPanel panel = new JPanel(new HorizontalLayout(2));
-        panel.setLayout(new HorizontalLayout(2));
-        panel.add(new JLabel("In-Situ Database:"));
-        panel.add(new JComboBox<>(new String[]{"Mermaid"}));
-        return panel;
-    }
-
-    private JPanel createDatabaseSelectionPanel() {
-        Locale.setDefault(Locale.ENGLISH);
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         final Calendar utcCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         utcCalendar.set(Calendar.HOUR_OF_DAY, 12);
         utcCalendar.set(Calendar.MINUTE, 0);
@@ -56,46 +38,58 @@ public class InsituClientForm extends JPanel {
         layout.setTableFill(TableLayout.Fill.HORIZONTAL);
         layout.setTableAnchor(TableLayout.Anchor.NORTHWEST);
         layout.setTablePadding(4, 4);
-        layout.setTableWeightX(1.0);
+        layout.setTableWeightX(0.1);
+        layout.setTableWeightY(0.0);
 
-        layout.setCellWeightX(0, 0, 0.0);
+        setLayout(layout);
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+
+        layout.setCellWeightX(0, 1, 1.0);
         layout.setCellColspan(0, 1, 3);
-        layout.setCellWeightX(0, 1, 2.0);
-        layout.setCellWeightX(1, 1, 1.3);
-        layout.setCellWeightX(1, 3, 1.3);
-        layout.setCellColspan(2, 1, 3);
-        final JPanel panel = new JPanel(layout);
-        panel.setBorder(new EmptyBorder(4, 4, 4, 4));
-        panel.add(new JLabel("In-Situ Database:"));
-        panel.add(new JComboBox<>(new String[]{"MERMAID"}));
-        panel.add(new JLabel("Campaign:"));
+        add(new JLabel("In-Situ Database:"));
+        add(new JComboBox<>(new String[]{"MERMAID"}));
+
+        layout.setCellWeightX(1, 1, 1.0);
+        layout.setCellWeightX(1, 3, 1.0);
+        layout.setCellFill(1, 1, TableLayout.Fill.BOTH);
+        layout.setCellFill(1, 3, TableLayout.Fill.BOTH);
+        layout.setRowWeightY(1, 1.0);
+        add(new JLabel("Campaign:"));
         final JList<String> campaignList = new JList<>(new String[]{"BUSSOLE", "AERONET"});
         campaignList.setVisibleRowCount(6);
-        panel.add(new JScrollPane(campaignList));
-        panel.add(new JLabel("Parameter:"));
+        add(new JScrollPane(campaignList));
+        add(new JLabel("Parameter:"));
         final JList<String> paramList = new JList<>(new String[]{
                 "chlorophyll_a_total", "es_412", "es_443", "es_490", "es_510", "es_560", "es_665",
                 "es_683", "kd_412", "kd_443", "kd_490", "kd_510", "kd_560", "kd_665", "kd_683", "ozone_concentration"
         });
         paramList.setVisibleRowCount(6);
-        panel.add(new JScrollPane(paramList));
-        panel.add(new JLabel("Product:"));
-        panel.add(new JComboBox<>(new String[]{"S3A_OL_2_WFR____20100602T094537_20100602T094837_2015070..."}));
-        panel.add(new JLabel("Start time:"));
+        add(new JScrollPane(paramList));
+
+        layout.setCellWeightX(2, 1, 1.0);
+        layout.setCellColspan(2, 1, 3);
+        add(new JLabel("Product:"));
+        add(new JComboBox<>(new String[]{"S3A_OL_2_WFR____20100602T094537_20100602T094837_2015070..."}));
+
+        add(new JLabel("Start time:"));
         utcCalendar.add(Calendar.DAY_OF_YEAR, -1);
-        panel.add(new DateTimePicker(utcCalendar.getTime(), Locale.getDefault(),
-                                     new SimpleDateFormat("dd-MMM-yyyy"), new SimpleDateFormat("HH:mm:ss")));
+        add(new DateTimePicker(utcCalendar.getTime(), Locale.getDefault(),
+                                     new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss"), new SimpleDateFormat("HH:mm:ss")));
         utcCalendar.add(Calendar.DAY_OF_YEAR, 2);
-        panel.add(new JLabel("End time:"));
-        panel.add(new DateTimePicker(utcCalendar.getTime(), Locale.getDefault(), new SimpleDateFormat("dd-MMM-yyyy"),
+        add(new JLabel("End time:"));
+        add(new DateTimePicker(utcCalendar.getTime(), Locale.getDefault(), new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss"),
                                      new SimpleDateFormat("HH:mm:ss")));
 
+        add(new JLabel("Longitude Minimum:"));
+        add(new JFormattedTextField());
 
-        return panel;
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            Locale.setDefault(Locale.ENGLISH);
+            TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
             JFrame frame = new JFrame();
             frame.setTitle("In-Siut Data Access");
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
