d360bdd7bcd9f7521f10068aa757b728babee438
marpet <marco.peters@brockmann-consult.de>
2016-02-12 11:29:28 +0100

    cleaned up the code a bit

--- s3tbx-watermask/src/main/java/org/esa/s3tbx/watermask/operator/WatermaskConstants.java
+++ s3tbx-watermask/src/main/java/org/esa/s3tbx/watermask/operator/WatermaskConstants.java
@@ -5,32 +5,16 @@ import org.esa.snap.core.util.SystemUtils;
 import java.nio.file.Path;
 
 /**
- * todo: add comment
- * To change this template use File | Settings | File Templates.
- * Date: 23.12.2015
- * Time: 15:04
- *
+ * Holds static constants for retrieving auxdata from the remote host.
  * @author olafd
  */
 public class WatermaskConstants {
-//    public static final String AUXDATA_VERSION = "3.0.0";
-//    public static final String AUXDATA_FILE_EXT = ".zip";
-
-    // test setup, not (yet) used
-//    public static final String REMOTE_FTP_HOST = "ftp.brockmann-consult.de";
-//    public static final String REMOTE_FTP_PATH = "/temp/olaf/s3tbx-watermask-aux/auxdata_" + AUXDATA_VERSION + "/images/";
-//    public static final String FTP_USER = "diversity";
-
-//    public static final String FTP_PASSWORD = "diversity!";
-    // preliminary test HTTP location:
-//    public static final String REMOTE_HTTP_HOST = "http://gws-access.cems.rl.ac.uk";
-//    public static final String REMOTE_HTTP_PATH = "/public/globalbedo/olafd/s3tbx-watermask-aux_" + AUXDATA_VERSION + "/images/";
     // the 'preliminary final' HTTP location (might get a versioning later, tbd):
     public static final String REMOTE_HTTP_HOST = "http://step.esa.int";
 
     public static final String REMOTE_HTTP_PATH = "/auxdata/watermask/images/";
 
-    public static final Path LOCAL_AUXDATA_PATH = SystemUtils.getAuxDataPath().resolve("s3tbx/watermask").toAbsolutePath();
+    public static final Path LOCAL_AUXDATA_PATH = SystemUtils.getAuxDataPath().resolve("watermask").toAbsolutePath();
 
     // we have:
     // 50m.zip
