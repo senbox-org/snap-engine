6a10c711d3389f5136174641deb5f07a6b82dc71
marpet <marco.peters@brockmann-consult.de>
2016-02-19 17:59:16 +0100

    finalised test

--- s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/QueryBuilder.java
+++ s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/QueryBuilder.java
@@ -1,20 +1,39 @@
 package org.esa.s3tbx.insitu;
 
 import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.util.StringUtils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Marco Peters
  */
 public class QueryBuilder {
 
+    private static final String PARAM_LON_MIN = "lonMin";
+    private static final String PARAM_LAT_MIN = "latMin";
+    private static final String PARAM_LON_MAX = "lonMax=";
+    private static final String PARAM_LAT_MAX = "latMax";
+    private static final String PARAM_START_DATE = "startDate";
+    private static final String PARAM_STOP_DATE = "stopDate";
+    private static final String PARAM_PARAM = "param";
+    private static final String PARAM_CAMPAIGN = "campaign";
+    private static final String PARAM_SHIFT = "shift";
+    private static final String PARAM_LIMIT = "limit";
+    private static final String PARAM_COUNT_ONLY = "count_only";
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     private String subject;
-    private double lonMin;
-    private double latMin;
-    private double lonMax;
-    private double latMax;
+    private Double lonMin;
+    private Double latMin;
+    private Double lonMax;
+    private Double latMax;
     private ProductData.UTC startDate;
     private ProductData.UTC stopDate;
-    private String[] parameters;
+    private String[] param;
     private String campaign;
     private int shift;
     private int limit;
@@ -25,19 +44,19 @@ public class QueryBuilder {
         this.subject = subject;
     }
 
-    public void lonMin(double lonMin) {
+    public void lonMin(Double lonMin) {
         this.lonMin = lonMin;
     }
 
-    public void latMin(double latMin) {
+    public void latMin(Double latMin) {
         this.latMin = latMin;
     }
 
-    public void lonMax(double lonMax) {
+    public void lonMax(Double lonMax) {
         this.lonMax = lonMax;
     }
 
-    public void latMax(double latMax) {
+    public void latMax(Double latMax) {
         this.latMax = latMax;
     }
 
@@ -50,7 +69,7 @@ public class QueryBuilder {
     }
 
     public void param(String[] parameters) {
-        this.parameters = parameters;
+        this.param = parameters;
     }
 
     public void campaign(String campaign) {
@@ -69,4 +88,54 @@ public class QueryBuilder {
         this.countOnly = countOnly;
     }
 
+    public String createQuery() {
+        List<String> queryParams= new ArrayList<>();
+
+        if(lonMin != null) {
+            queryParams.add(PARAM_LON_MIN + "=" + lonMin);
+        }
+        if(latMin != null) {
+            queryParams.add(PARAM_LAT_MIN + "=" + latMin);
+        }
+        if(lonMax != null) {
+            queryParams.add(PARAM_LON_MAX + lonMax);
+        }
+        if(latMax != null) {
+            queryParams.add(PARAM_LAT_MAX + "=" + latMax);
+        }
+        if(startDate != null) {
+            queryParams.add(PARAM_START_DATE + "=" + DATE_FORMAT.format(startDate.getAsDate()));
+        }
+        if(stopDate != null) {
+            queryParams.add(PARAM_STOP_DATE + "=" + DATE_FORMAT.format(stopDate.getAsDate()));
+        }
+        if(param != null) {
+            queryParams.add(PARAM_PARAM + "=" + StringUtils.arrayToCsv(param));
+        }
+        if(campaign != null) {
+            queryParams.add(PARAM_CAMPAIGN + "=" + campaign);
+        }
+        if(shift > 0) {
+            queryParams.add(PARAM_SHIFT + "=" + shift);
+        }
+        if(limit > 0) {
+            queryParams.add(PARAM_LIMIT + "=" + limit);
+        }
+        if(countOnly) {
+            queryParams.add(PARAM_COUNT_ONLY);
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        sb.append("/").append(subject).append("?");
+
+        for (int i = 0; i < queryParams.size(); i++) {
+            String queryParam = queryParams.get(i);
+            sb.append(queryParam);
+            if(i < queryParams.size()-1) {
+                sb.append("&");
+            }
+        }
+
+        return sb.toString();
+    }
 }
--- s3tbx-insitu-client-ui/src/test/java/org/esa/s3tbx/insitu/QueryBuilderTest.java
+++ s3tbx-insitu-client-ui/src/test/java/org/esa/s3tbx/insitu/QueryBuilderTest.java
@@ -1,11 +1,24 @@
 package org.esa.s3tbx.insitu;
 
+import org.esa.snap.core.datamodel.ProductData;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import static org.junit.Assert.*;
 
 /**
  * @author Marco Peters
  */
 public class QueryBuilderTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Locale.setDefault(Locale.ENGLISH);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     @Test
     public void testQueryCreation() throws Exception {
@@ -15,7 +28,28 @@ public class QueryBuilderTest {
         builder.latMax(46.12);
         builder.lonMin(5.0);
         builder.lonMax(15.36);
-
+        builder.startDate(ProductData.UTC.parse("01-Jan-2014 00:00:00"));
+        builder.stopDate(ProductData.UTC.parse("31-Dec-2015 00:00:00"));
+        builder.param(new String[]{"param1", "param2", "param3"});
+        builder.limit(10);
+        builder.shift(5);
+        builder.countOnly(true);
+        String query = builder.createQuery();
+        assertTrue(query.startsWith("/interest?"));
+        assertTrue(query.contains("campaign=Muscheln"));
+        assertTrue(query.contains("latMin=-10.943"));
+        assertTrue(query.contains("lonMin=5.0"));
+        assertTrue(query.contains("latMax=46.12"));
+        assertTrue(query.contains("lonMax=15.36"));
+        assertTrue(query.contains("startDate=2014-01-01 00:00:00"));
+        assertTrue(query.contains("stopDate=2015-12-31 00:00:00"));
+        assertTrue(query.contains("param=param1,param2,param3"));
+        assertTrue(query.contains("limit=10"));
+        assertTrue(query.contains("shift=5"));
+        assertTrue(query.contains("count_only"));
+        assertEquals(10, query.chars().filter(value -> value == '&').count());
+        final int lastIndex = query.length() - 1;
+        assertTrue(query.charAt(lastIndex) != '&');
 
     }
 }
