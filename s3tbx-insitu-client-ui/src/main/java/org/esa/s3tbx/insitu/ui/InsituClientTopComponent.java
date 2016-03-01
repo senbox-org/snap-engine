055aae6ed43c563e0bd38356162f5011372767e3
marpet <marco.peters@brockmann-consult.de>
2016-03-01 10:39:55 +0100

    enabled TopComponent again

--- s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientTopComponent.java
+++ s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientTopComponent.java
@@ -15,24 +15,33 @@
  */
 package org.esa.s3tbx.insitu.ui;
 
+import com.bc.ceres.swing.TableLayout;
+import org.esa.snap.rcp.actions.help.HelpAction;
+import org.esa.snap.tango.TangoIcons;
+import org.esa.snap.ui.tool.ToolButtonFactory;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
 
+import javax.swing.AbstractButton;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Component;
 
-//@TopComponent.Description(
-//        preferredID = "InsituClientTopComponent",
-//        iconBase = "org/esa/s3tbx/insitu/insitu24.png",
-//        persistenceType = TopComponent.PERSISTENCE_ALWAYS
-//)
-//@TopComponent.Registration(mode = "rightSlidingSide", openAtStartup = false, position = 150)
-//@ActionID(category = "Window", id = "InsituClientTopComponent")
-//@ActionReference(path = "Menu/View/Tool Windows")
-//@TopComponent.OpenActionRegistration(
-//        displayName = "#CTL_InsituClientTopComponent_Name",
-//        preferredID = "InsituClientTopComponent"
-//)
+@TopComponent.Description(
+        preferredID = "InsituClientTopComponent",
+        iconBase = "org/esa/s3tbx/insitu/insitu.png",
+        persistenceType = TopComponent.PERSISTENCE_ALWAYS
+)
+@TopComponent.Registration(mode = "explorer", openAtStartup = false, position = 150)
+@ActionID(category = "Window", id = "InsituClientTopComponent")
+@ActionReference(path = "Menu/View/Tool Windows")
+@TopComponent.OpenActionRegistration(
+        displayName = "#CTL_InsituClientTopComponent_Name",
+        preferredID = "InsituClientTopComponent"
+)
 @NbBundle.Messages({"CTL_InsituClientTopComponent_Name=In-Situ Data Access"})
 public class InsituClientTopComponent extends TopComponent implements HelpCtx.Provider {
 
@@ -40,9 +49,36 @@ public class InsituClientTopComponent extends TopComponent implements HelpCtx.Pr
 
     public InsituClientTopComponent() {
         setName(Bundle.CTL_InsituClientTopComponent_Name());
-        InsituClientForm icf = new InsituClientForm();
         setLayout(new BorderLayout());
-        add(icf, BorderLayout.CENTER);
+        add(new InsituClientForm(), BorderLayout.CENTER);
+        add(createStatusPanel(new HelpCtx(HELP_ID)), BorderLayout.SOUTH);
+    }
+
+    private Component createStatusPanel(HelpCtx helpCtx) {
+        final TableLayout layout = new TableLayout(4);
+        layout.setTableFill(TableLayout.Fill.HORIZONTAL);
+        layout.setTableWeightX(0.5);
+        layout.setCellWeightX(0, 2, 2.0);
+        layout.setCellWeightX(0, 3, 0.0);
+        layout.setTableAnchor(TableLayout.Anchor.NORTHWEST);
+        layout.setTablePadding(4, 4);
+
+        final JPanel contentPanel = new JPanel(layout);
+        final AbstractButton refreshButton = ToolButtonFactory.createButton(TangoIcons.actions_view_refresh(TangoIcons.Res.R22), false);
+        refreshButton.setText("#Obs: 486");
+        refreshButton.setName("refreshButton");
+        contentPanel.add(refreshButton);
+        final AbstractButton downloadButton = ToolButtonFactory.createButton(TangoIcons.actions_document_save(TangoIcons.Res.R22), false);
+        downloadButton.setText("Download");
+        downloadButton.setName("downloadButton");
+        contentPanel.add(downloadButton);
+        contentPanel.add(layout.createHorizontalSpacer());
+
+        AbstractButton helpButton = ToolButtonFactory.createButton(new HelpAction(helpCtx), false);
+        helpButton.setName("helpButton");
+        contentPanel.add(helpButton);
+
+        return contentPanel;
     }
 
     @Override
