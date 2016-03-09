fe304659efcc4756f8710518f348066f0dd1a3df
marpet <marco.peters@brockmann-consult.de>
2016-03-09 12:53:43 +0100

    set anchor to northwest

--- s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientForm.java
+++ s3tbx-insitu-client-ui/src/main/java/org/esa/s3tbx/insitu/ui/InsituClientForm.java
@@ -72,9 +72,9 @@ class InsituClientForm extends JPanel {
         layout.setCellWeightX(1, 1, 1.0);
         layout.setCellWeightX(1, 3, 1.0);
         layout.setCellFill(1, 1, TableLayout.Fill.BOTH);
-        layout.setCellAnchor(1, 1, TableLayout.Anchor.NORTHWEST);
+        layout.setCellAnchor(1, 0, TableLayout.Anchor.NORTHWEST);
         layout.setCellFill(1, 3, TableLayout.Fill.BOTH);
-        layout.setCellAnchor(1, 3, TableLayout.Anchor.NORTHWEST);
+        layout.setCellAnchor(1, 2, TableLayout.Anchor.NORTHWEST);
         layout.setRowWeightY(1, 0.6);
         add(new JLabel("Dataset:"));
         final JList<InsituDataset> datasetList = new JList<>(model.getDatasetModel());
@@ -92,7 +92,7 @@ class InsituClientForm extends JPanel {
         layout.setCellWeightX(2, 1, 1.0);
         layout.setCellColspan(2, 1, 3);
         layout.setCellFill(2, 1, TableLayout.Fill.BOTH);
-        layout.setCellAnchor(2, 1, TableLayout.Anchor.NORTHWEST);
+        layout.setCellAnchor(2, 0, TableLayout.Anchor.NORTHWEST);
         layout.setCellWeightY(2, 1, 1.0);
         add(new JLabel("Product:"));
         final JList<Product> productList = new JList<>(model.getProductListModel());
