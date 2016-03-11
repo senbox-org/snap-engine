4d1647dbbf433c7cfd0b9b7f4032501bb89d7bdb
marpet <marco.peters@brockmann-consult.de>
2016-03-11 15:06:22 +0100

    added javadoc

--- s3tbx-idepix/src/main/java/org/esa/s3tbx/idepix/operators/BasisOp.java
+++ s3tbx-idepix/src/main/java/org/esa/s3tbx/idepix/operators/BasisOp.java
@@ -4,24 +4,22 @@ import org.esa.snap.core.datamodel.Mask;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorSpi;
-import org.esa.snap.core.gpf.annotations.OperatorMetadata;
 import org.esa.snap.core.util.ProductUtils;
 
 public abstract class BasisOp extends Operator {
 
     /**
-     * creates a new product with the same size
+     * Creates a new product with the same size
      *
-     * @param sourceProduct
-     * @param name
-     * @param type
+     * @param sourceProduct the source product
+     * @param name the name of the newly created product
+     * @param type the product type of the newly created product
      *
      * @return targetProduct
      */
     public Product createCompatibleProduct(Product sourceProduct, String name, String type) {
         final int sceneWidth = sourceProduct.getSceneRasterWidth();
         final int sceneHeight = sourceProduct.getSceneRasterHeight();
-
         Product targetProduct = new Product(name, type, sceneWidth, sceneHeight);
         copyProductTrunk(sourceProduct, targetProduct);
         return targetProduct;
@@ -29,14 +27,19 @@ public abstract class BasisOp extends Operator {
 
     /**
      * Copies basic information for a MERIS product to the target product
+     * To the basic information belong the tie-point grids and the geo-coding.
      *
-     * @param sourceProduct
-     * @param targetProduct
+     * @param sourceProduct the source product
+     * @param targetProduct the target product to get the information from
      */
     public void copyProductTrunk(Product sourceProduct,
                                  Product targetProduct) {
-        copyTiePoints(sourceProduct, targetProduct);
-        copyBaseGeoInfo(sourceProduct, targetProduct);
+        // copy all tie point grids to output product
+        ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
+        // copy geo-coding to the output product
+        ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
+        targetProduct.setStartTime(sourceProduct.getStartTime());
+        targetProduct.setEndTime(sourceProduct.getEndTime());
     }
 
     public void renameL1bMaskNames(Product product) {
@@ -58,32 +61,6 @@ public abstract class BasisOp extends Operator {
         }
     }
 
-
-    /**
-     * Copies the tie point data.
-     *
-     * @param sourceProduct
-     * @param targetProduct
-     */
-    private void copyTiePoints(Product sourceProduct,
-                               Product targetProduct) {
-        // copy all tie point grids to output product
-        ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
-    }
-
-    /**
-     * Copies geocoding and the start and stop time.
-     *
-     * @param sourceProduct
-     * @param targetProduct
-     */
-    private void copyBaseGeoInfo(Product sourceProduct,
-                                 Product targetProduct) {
-        // copy geo-coding to the output product
-        ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
-        targetProduct.setStartTime(sourceProduct.getStartTime());
-        targetProduct.setEndTime(sourceProduct.getEndTime());
-    }
 
     /**
      * The Service Provider Interface (SPI) for the operator.
