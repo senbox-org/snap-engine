25760cb1eaa56538c4818566b85e986a0010d314
TonioF <tonio.fincke@brockmann-consult.de>
2016-03-13 12:40:32 +0100

    do not transfer geocoding

--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/slstr/SlstrTiePointGeoCoding.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/slstr/SlstrTiePointGeoCoding.java
@@ -1,9 +1,7 @@
 package org.esa.s3tbx.dataio.s3.slstr;
 
-import org.esa.snap.core.datamodel.GeoPos;
-import org.esa.snap.core.datamodel.PixelPos;
-import org.esa.snap.core.datamodel.TiePointGeoCoding;
-import org.esa.snap.core.datamodel.TiePointGrid;
+import org.esa.snap.core.dataio.ProductSubsetDef;
+import org.esa.snap.core.datamodel.*;
 import org.geotools.referencing.operation.transform.AffineTransform2D;
 
 import java.awt.geom.AffineTransform;
@@ -40,4 +38,8 @@ public class SlstrTiePointGeoCoding extends TiePointGeoCoding {
         return transformedPixelPos;
     }
 
+    @Override
+    public boolean transferGeoCoding(Scene srcScene, Scene destScene, ProductSubsetDef subsetDef) {
+        return false;
+    }
 }
