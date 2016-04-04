747288cff174552f23a4f9c28eae284a9825fcc8
marpet <marco.peters@brockmann-consult.de>
2016-04-04 13:37:24 +0200

    [SIIITBX-97] Subset of SeaDas L2 files not correct

--- seadas-reader/src/test/java/gov/nasa/gsfc/seadas/dataio/BowtiePixelGeoCodingTest.java
+++ seadas-reader/src/test/java/gov/nasa/gsfc/seadas/dataio/BowtiePixelGeoCodingTest.java
@@ -17,6 +17,10 @@
 package gov.nasa.gsfc.seadas.dataio;
 
 import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.dataio.ProductSubsetDef;
+import org.esa.snap.core.datamodel.GeoCoding;
+import org.esa.snap.core.datamodel.GeoPos;
+import org.esa.snap.core.datamodel.PixelPos;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.util.ProductUtils;
 import org.junit.Test;
@@ -41,5 +45,32 @@ public class BowtiePixelGeoCodingTest {
 
         assertNotNull(targetProduct.getSceneGeoCoding());
         assertTrue(targetProduct.getSceneGeoCoding() instanceof BowtiePixelGeoCoding);
+    }
+
+
+    @Test
+    public void testLatAndLonAreCorrectlySubsetted() throws URISyntaxException, IOException {
+        Product product = ProductIO.readProduct(new File(getClass().getResource("bowtiepixelgeocoding_test_product.L2_sub").toURI()));
+        GeoCoding sourcceGeoCoding = product.getSceneGeoCoding();
+        assertTrue(sourcceGeoCoding instanceof BowtiePixelGeoCoding);
+
+        ProductSubsetDef subsetDef = new ProductSubsetDef();
+        subsetDef.setRegion(50, 50, 10, 10);
+        subsetDef.addNodeName("chlor_a");
+        Product targetProduct = product.createSubset(subsetDef, "subset", "");
+
+        GeoCoding targetGeoCoding = targetProduct.getSceneGeoCoding();
+        assertNotNull(targetGeoCoding);
+        assertTrue(targetGeoCoding instanceof BowtiePixelGeoCoding);
+        assertTrue(targetProduct.containsBand("latitude"));
+        assertTrue(targetProduct.containsBand("longitude"));
+
+        PixelPos sourcePixelPos = new PixelPos(50.5, 50.5);
+        GeoPos expected = sourcceGeoCoding.getGeoPos(sourcePixelPos, new GeoPos());
+        PixelPos targetPixelPos = new PixelPos(0.5, 0.5);
+        GeoPos actual = targetGeoCoding.getGeoPos(targetPixelPos, new GeoPos());
+        assertEquals(expected.getLat(), actual.getLat(), 1.0e-6);
+        assertEquals(expected.getLon(), actual.getLon(), 1.0e-6);
+
     }
 }
