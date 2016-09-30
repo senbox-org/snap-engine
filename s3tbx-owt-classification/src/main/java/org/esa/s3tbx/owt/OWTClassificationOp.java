6b6494ce990926ddf7b0e2a8052c02334ae529b1
Muhammad <muhammshu@yahoo.com>
2016-09-30 09:15:13 +0200

    remove the scary error message.

--- s3tbx-owt-classification/src/main/java/org/esa/s3tbx/owt/OWTClassificationOp.java
+++ s3tbx-owt-classification/src/main/java/org/esa/s3tbx/owt/OWTClassificationOp.java
@@ -23,6 +23,7 @@ import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.IndexCoding;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.gpf.OperatorCancelException;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.OperatorSpi;
 import org.esa.snap.core.gpf.annotations.OperatorMetadata;
@@ -101,10 +102,11 @@ public class OWTClassificationOp extends PixelOperator {
         final Band[] bands = sourceProduct.getBands();
         String bestBandName = getBestBandName(reflectancesPrefix, wavelength, bands);
         if (bestBandName == null) {
-            throw new OperatorException(
+            throw new OperatorCancelException(
                     String.format("Not able to find band with prefix '%s' and wavelength '%4.3f'.",
                                   reflectancesPrefix, wavelength)
             );
+
         }
         return bestBandName;
     }
