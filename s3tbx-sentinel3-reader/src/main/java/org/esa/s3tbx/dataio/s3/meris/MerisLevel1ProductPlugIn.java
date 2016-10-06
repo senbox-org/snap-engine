224da6fee43a638d031349e7274a54a64154306f
marpet <marco.peters@brockmann-consult.de>
2016-10-06 15:58:42 +0200

    added RGB profiles for MERIS4

--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel1ProductPlugIn.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel1ProductPlugIn.java
@@ -2,6 +2,8 @@ package org.esa.s3tbx.dataio.s3.meris;
 
 import org.esa.s3tbx.dataio.s3.Sentinel3ProductReaderPlugIn;
 import org.esa.snap.core.dataio.ProductReader;
+import org.esa.snap.core.datamodel.RGBImageProfile;
+import org.esa.snap.core.datamodel.RGBImageProfileManager;
 
 /**
  * @author Tonio Fincke
@@ -9,6 +11,9 @@ import org.esa.snap.core.dataio.ProductReader;
 public class MerisLevel1ProductPlugIn extends Sentinel3ProductReaderPlugIn {
 
     private static final String format_name = "MER_L1_S3";
+    static {
+        registerRGBProfiles();
+    }
 
     public MerisLevel1ProductPlugIn() {
         super(format_name, "MERIS Level 1 in Sentinel-3 product format",
@@ -20,4 +25,33 @@ public class MerisLevel1ProductPlugIn extends Sentinel3ProductReaderPlugIn {
         return new MerisLevel1ProductReader(this);
     }
 
+    private static void registerRGBProfiles() {
+        RGBImageProfileManager manager = RGBImageProfileManager.getInstance();
+        manager.addProfile(new RGBImageProfile("MERIS4 L1b - Tristimulus",
+                                               new String[]{
+                                                       "log(1.0 + 0.35 * M02_radiance + 0.60 * M05_radiance + M06_radiance + 0.13 * M07_radiance)",
+                                                       "log(1.0 + 0.21 * M03_radiance + 0.50 * M04_radiance + M05_radiance + 0.38 * M06_radiance)",
+                                                       "log(1.0 + 0.21 * M01_radiance + 1.75 * M02_radiance + 0.47 * M03_radiance + 0.16 * M04_radiance)"
+                                               },
+                                               new String[]{
+                                                       "*ME_1*",
+                                                       "ENV_ME_1_*",
+                                                       "",
+                                               }
+        ));
+        manager.addProfile(new RGBImageProfile("MERIS4 L1b - 13,5,1",
+                                               new String[]{
+                                                       "M13_radiance",
+                                                       "M05_radiance",
+                                                       "M01_radiance"
+                                               }
+        ));
+        manager.addProfile(new RGBImageProfile("MERIS4 L1b - 13,4,2",
+                                               new String[]{
+                                                       "M13_radiance",
+                                                       "M04_radiance",
+                                                       "M02_radiance"
+                                               }
+        ));
+    }
 }
--- s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel2ProductPlugIn.java
+++ s3tbx-sentinel3-reader/src/main/java/org/esa/s3tbx/dataio/s3/meris/MerisLevel2ProductPlugIn.java
@@ -2,6 +2,8 @@ package org.esa.s3tbx.dataio.s3.meris;
 
 import org.esa.s3tbx.dataio.s3.Sentinel3ProductReaderPlugIn;
 import org.esa.snap.core.dataio.ProductReader;
+import org.esa.snap.core.datamodel.RGBImageProfile;
+import org.esa.snap.core.datamodel.RGBImageProfileManager;
 
 /**
  * @author Tonio Fincke
@@ -9,6 +11,10 @@ import org.esa.snap.core.dataio.ProductReader;
 public class MerisLevel2ProductPlugIn extends Sentinel3ProductReaderPlugIn {
 
     private static final String format_name = "MER_L2_S3";
+
+    static {
+        registerRGBProfiles();
+    }
 
     public MerisLevel2ProductPlugIn() {
         super(format_name, "MERIS Level 2 in Sentinel-3 product format",
@@ -18,5 +24,28 @@ public class MerisLevel2ProductPlugIn extends Sentinel3ProductReaderPlugIn {
     @Override
     public ProductReader createReaderInstance() {
         return new MerisLevel2ProductReader(this);
+    }
+
+    private static void registerRGBProfiles() {
+        RGBImageProfileManager manager = RGBImageProfileManager.getInstance();
+        manager.addProfile(new RGBImageProfile("MERIS L2 - Tristimulus",
+                                               new String[]{
+                                                       "log(0.05 + 0.35 * M02_rho_TOA + 0.60 * M05_rho_TOA + M06_rho_TOA + 0.13 * M07_rho_TOA)",
+                                                       "log(0.05 + 0.21 * M03_rho_TOA + 0.50 * M04_rho_TOA + M05_rho_TOA + 0.38 * M06_rho_TOA)",
+                                                       "log(0.05 + 0.21 * M01_rho_TOA + 1.75 * M02_rho_TOA + 0.47 * M03_rho_TOA + 0.16 * M04_rho_TOA)"
+                                               },
+                                               new String[]{
+                                                       "*ME_2_*",
+                                                       "ENV_ME_2_*",
+                                                       "",
+                                               }
+        ));
+        manager.addProfile(new RGBImageProfile("MERIS4 L2 - 13,5,1",
+                                               new String[]{
+                                                       "M13_rho_TOA",
+                                                       "M05_rho_TOA",
+                                                       "M01_rho_TOA"
+                                               }
+        ));
     }
 }
