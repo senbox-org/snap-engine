fc65323b6b4b95e9292d2782a67e256560758153
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-05-09 13:06:29 +0200

    revert to not using OperatorProduct

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/pointop/PointOperator.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/pointop/PointOperator.java
@@ -37,7 +37,6 @@ import org.esa.snap.core.datamodel.VirtualBand;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.Tile;
-import org.esa.snap.core.gpf.internal.OperatorProduct;
 import org.esa.snap.core.image.ResolutionLevel;
 import org.esa.snap.core.image.VirtualBandOpImage;
 import org.esa.snap.core.jexp.Term;
@@ -142,8 +141,8 @@ public abstract class PointOperator extends Operator {
     protected Product createTargetProduct() throws OperatorException {
         Product sourceProduct = getSourceProduct();
         Assert.state(sourceProduct != null, "source product not set");
-        return new OperatorProduct(this, new Product(getId(), getClass().getName(),
-                sourceProduct.getSceneRasterWidth(), sourceProduct.getSceneRasterHeight()));
+        return new Product(getId(), getClass().getName(),
+                sourceProduct.getSceneRasterWidth(), sourceProduct.getSceneRasterHeight());
     }
 
     /**
