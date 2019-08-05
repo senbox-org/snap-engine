a8cefabce2970bc51b528904e48312c4a3f2b81e
jeancoravu <jean.coravu@c-s.ro>
2019-08-05 11:14:17 +0300

    Create new package 'org.esa.snap.product.library.v2' to store the new classes of the product library option.

---
+++ snap-product-library/src/main/java/org/esa/snap/product/library/v2/TestDataSource.java
@@ -0,0 +1,11 @@
+package org.esa.snap.product.library.v2;
+
+/**
+ * Created by jcoravu on 5/8/2019.
+ */
+public class TestDataSource {
+
+    public static void main(String args[]) {
+        System.out.println("TestDataSource for product library.");
+    }
+}
