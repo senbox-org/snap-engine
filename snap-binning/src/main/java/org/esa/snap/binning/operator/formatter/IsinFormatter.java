b6fd889682b3f05a513b367b7d2b683b5b34de03
TomBlock <tom.block@brockmann-consult.de>
2019-09-18 17:09:00 +0200

    intermediate

--- snap-binning/src/main/java/org/esa/snap/binning/operator/formatter/IsinFormatter.java
+++ snap-binning/src/main/java/org/esa/snap/binning/operator/formatter/IsinFormatter.java
@@ -9,6 +9,7 @@ import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.MetadataElement;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.datamodel.RasterDataNode;
 import org.esa.snap.core.util.grid.isin.IsinPoint;
 import org.esa.snap.core.util.grid.isin.IsinUtils;
 
@@ -16,8 +17,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Iterator;
-
-import static org.esa.snap.core.util.grid.isin.IsinAPI.Raster.GRID_500_M;
 
 public class IsinFormatter implements Formatter {
 
@@ -40,18 +39,19 @@ public class IsinFormatter implements Formatter {
         if (featureNames.length == 0) {
             throw new IllegalArgumentException("Illegal binning context: featureNames.length == 0");
         }
+        final ProductData[] targetVariables = new ProductData[featureNames.length];
+
+        final IsinPlanetaryGrid isinGrid = (IsinPlanetaryGrid) planetaryGrid;
 
         final String outputFormat = formatterConfig.getOutputFormat();
         final File outputFile = new File(formatterConfig.getOutputFile());
-        if (outputFile.exists() && outputFile.isDirectory()) {
-            // check if exists - overwrite?
-        }
 
         // iterate bin cells (check for tile index increment)
         int x_tile = -1;
         int y_tile = -1;
         String productName = null;
         Product tileProduct = null;
+        int productWidth = 0;
 
         final int partCount = temporalBinSource.open();
         for (int i = 0; i < partCount; i++) {
@@ -62,24 +62,41 @@ public class IsinFormatter implements Formatter {
                 final long index = temporalBin.getIndex();
                 final IsinPoint gridIndex = IsinPlanetaryGrid.toIsinPoint(index);
 
+                // make switch to new tile
+                // -----------------------
                 if (x_tile != gridIndex.getTile_col() || y_tile != gridIndex.getTile_line()) {
                     x_tile = gridIndex.getTile_col();
                     y_tile = gridIndex.getTile_line();
 
                     if (tileProduct != null) {
                         writeTileProduct(outputFormat, outputFile, productName, tileProduct);
+                        tileProduct.dispose();
                     }
 
-                    // @todo 1 tb/tb read resolution from config 2019-09-18
-                    tileProduct = IsinUtils.createMpcVegetationPrototype(x_tile, y_tile, GRID_500_M);
+                    tileProduct = IsinUtils.createMpcVegetationPrototype(x_tile, y_tile, isinGrid.getRaster());
+                    productWidth = tileProduct.getSceneRasterWidth();
                     productName = getProductName(startTime.getAsDate(), stopTime.getAsDate(), x_tile, y_tile);
+
+
+                    for (int feat = 0; feat < featureNames.length; feat++) {
+                        final RasterDataNode rasterDataNode = tileProduct.getRasterDataNode(featureNames[feat]);
+                        targetVariables[feat] = rasterDataNode.getRasterData();
+                    }
                 }
 
-
+                int writeIndex = (int) (gridIndex.getX() + productWidth * gridIndex.getY());
+                // @todo 1 tb/tb set correct feature values in test-setup 2019-09-18
+//                final float[] featureValues = temporalBin.getFeatureValues();
+//                for (int feat = 0; feat < featureValues.length; feat++) {
+//                    targetVariables[feat].setElemFloatAt(writeIndex, featureValues[feat]);
+//                }
             }
         }
 
-        writeTileProduct(outputFormat, outputFile, productName, tileProduct);
+        if (tileProduct != null) {
+            writeTileProduct(outputFormat, outputFile, productName, tileProduct);
+            tileProduct.dispose();
+        }
 
         // -- create file name from conventions
         // -- create product template
@@ -92,6 +109,5 @@ public class IsinFormatter implements Formatter {
     private void writeTileProduct(String outputType, File outputFile, String productName, Product tileProduct) throws IOException {
         final File targetFile = new File(outputFile, productName);
         ProductIO.writeProduct(tileProduct, targetFile.getAbsolutePath(), outputType);
-        tileProduct.dispose();
     }
 }
