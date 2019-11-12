99063095ed779244ff7a9942e1d031d391dd8eb9
dstefanescu <denisa.stefanescu@c-s.ro>
2019-11-12 16:36:54 +0200

    update the JP2 product reader in order to read a specific subset from the input product (SIITBX-373 Parameters for Reader Plugins)

--- snap-core/src/main/java/org/esa/snap/core/dataio/MetadataInspector.java
+++ snap-core/src/main/java/org/esa/snap/core/dataio/MetadataInspector.java
@@ -1,5 +1,8 @@
 package org.esa.snap.core.dataio;
 
+import org.esa.snap.core.datamodel.GeoCoding;
+import org.esa.snap.core.datamodel.GeoPos;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashSet;
@@ -20,6 +23,17 @@ public interface MetadataInspector {
     class Metadata {
         private Set<String> bandList = new HashSet<>();
         private Set<String> maskList = new HashSet<>();
+
+        private String productWidth;
+        private String productHeight;
+
+        private String latitudeNorth;
+        private String longitudeWest;
+        private String latitudeSouth;
+        private String longitudeEast;
+
+        private GeoCoding geoCoding;
+
         private boolean hasMasks;
 
         public Set<String> getBandList() {
@@ -44,6 +58,62 @@ public interface MetadataInspector {
 
         public void setHasMasks(boolean hasMasks) {
             this.hasMasks = hasMasks;
+        }
+
+        public String getProductWidth() {
+            return productWidth;
+        }
+
+        public void setProductWidth(String productWidth) {
+            this.productWidth = productWidth;
+        }
+
+        public String getProductHeight() {
+            return productHeight;
+        }
+
+        public void setProductHeight(String productHeight) {
+            this.productHeight = productHeight;
+        }
+
+        public String getLatitudeNorth() {
+            return latitudeNorth;
+        }
+
+        public void setLatitudeNorth(String latitudeNorth) {
+            this.latitudeNorth = latitudeNorth;
+        }
+
+        public String getLongitudeWest() {
+            return longitudeWest;
+        }
+
+        public void setLongitudeWest(String longitudeWest) {
+            this.longitudeWest = longitudeWest;
+        }
+
+        public String getLatitudeSouth() {
+            return latitudeSouth;
+        }
+
+        public void setLatitudeSouth(String latitudeSouth) {
+            this.latitudeSouth = latitudeSouth;
+        }
+
+        public String getLongitudeEast() {
+            return longitudeEast;
+        }
+
+        public void setLongitudeEast(String longitudeEast) {
+            this.longitudeEast = longitudeEast;
+        }
+
+        public GeoCoding getGeoCoding() {
+            return geoCoding;
+        }
+
+        public void setGeoCoding(GeoCoding geoCoding) {
+            this.geoCoding = geoCoding;
         }
     }
 }
