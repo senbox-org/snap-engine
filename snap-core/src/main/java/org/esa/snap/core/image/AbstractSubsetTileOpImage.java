251313a1b5cf32b86b39c1adb70776e90a277c01
jeancoravu <jean.coravu@c-s.ro>
2019-12-23 15:47:31 +0200

    Code refactoring in the RapidEye L1 product reader to read the bands based on a subset region.

--- snap-core/src/main/java/org/esa/snap/core/image/AbstractSubsetTileOpImage.java
+++ snap-core/src/main/java/org/esa/snap/core/image/AbstractSubsetTileOpImage.java
@@ -1,9 +1,13 @@
 package org.esa.snap.core.image;
 
 import com.bc.ceres.glevel.MultiLevelModel;
+import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.util.ImageUtils;
 
 import java.awt.*;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
 
 /**
  * Created by jcoravu on 11/12/2019.
@@ -13,31 +17,28 @@ public abstract class AbstractSubsetTileOpImage extends SingleBandedOpImage {
     protected final LevelImageSupport levelImageSupport;
     protected final Point imageOffset;
     protected final Point levelTileOffset;
-    protected final int bandIndex;
 
-    protected AbstractSubsetTileOpImage(MultiLevelModel imageMultiLevelModel, int dataBufferType, int bandIndex, Rectangle imageBounds, Dimension tileSize, Point tileOffset, int level) {
-        this(dataBufferType, bandIndex, imageBounds, tileSize, ImageUtils.computeTileDimensionAtResolutionLevel(tileSize, level),
+    protected AbstractSubsetTileOpImage(MultiLevelModel imageMultiLevelModel, int dataBufferType, Rectangle visibleImageBounds, Dimension tileSize, Point tileOffset, int level) {
+        this(dataBufferType, visibleImageBounds, tileSize, ImageUtils.computeTileDimensionAtResolutionLevel(tileSize, level),
                 tileOffset, ResolutionLevel.create(imageMultiLevelModel, level));
     }
 
-    private AbstractSubsetTileOpImage(int dataBufferType, int bandIndex, Rectangle imageBounds, Dimension tileSize, Dimension subTileSize, Point tileOffset, ResolutionLevel resolutionLevel) {
+    private AbstractSubsetTileOpImage(int dataBufferType, Rectangle visibleImageBounds, Dimension tileSize, Dimension subTileSize, Point tileOffset, ResolutionLevel resolutionLevel) {
         super(dataBufferType, null, tileSize.width, tileSize.height, subTileSize, null, resolutionLevel);
 
-        this.bandIndex = bandIndex;
         this.levelTileOffset = new Point(ImageUtils.computeLevelSize(tileOffset.x, resolutionLevel.getIndex()), ImageUtils.computeLevelSize(tileOffset.y, resolutionLevel.getIndex()));
-        this.imageOffset = new Point(imageBounds.x, imageBounds.y);
-        this.levelImageSupport = new LevelImageSupport(imageBounds.width, imageBounds.height, resolutionLevel);
+        this.imageOffset = new Point(visibleImageBounds.x, visibleImageBounds.y);
+        this.levelImageSupport = new LevelImageSupport(visibleImageBounds.width, visibleImageBounds.height, resolutionLevel);
     }
 
-    protected final Rectangle computeIntersection(Rectangle destinationRectangle) {
+    protected final Rectangle computeIntersectionOnNormalBounds(Rectangle levelDestinationRectangle) {
         int sourceImageWidth = getImageWidth();
         int sourceImageHeight = getImageHeight();
-        Rectangle sourceImageBounds = new Rectangle(this.imageOffset.x, this.imageOffset.y, sourceImageWidth, sourceImageHeight);
 
-        int destinationSourceWidth = this.levelImageSupport.getSourceWidth(destinationRectangle.width);
-        int destinationSourceHeight = this.levelImageSupport.getSourceHeight(destinationRectangle.height);
-        int destinationSourceX = this.levelImageSupport.getSourceX(this.levelTileOffset.x + destinationRectangle.x);
-        int destinationSourceY = this.levelImageSupport.getSourceY(this.levelTileOffset.y + destinationRectangle.y);
+        int destinationSourceWidth = this.levelImageSupport.getSourceWidth(levelDestinationRectangle.width);
+        int destinationSourceHeight = this.levelImageSupport.getSourceHeight(levelDestinationRectangle.height);
+        int destinationSourceX = this.levelImageSupport.getSourceX(this.levelTileOffset.x + levelDestinationRectangle.x);
+        int destinationSourceY = this.levelImageSupport.getSourceY(this.levelTileOffset.y + levelDestinationRectangle.y);
         if (destinationSourceX + destinationSourceWidth > sourceImageWidth) {
             destinationSourceWidth = sourceImageWidth - destinationSourceX;
         }
@@ -47,8 +48,61 @@ public abstract class AbstractSubsetTileOpImage extends SingleBandedOpImage {
         destinationSourceX += this.imageOffset.x;
         destinationSourceY += this.imageOffset.y;
 
-        Rectangle tileBoundsInSourceImage = new Rectangle(destinationSourceX, destinationSourceY, destinationSourceWidth, destinationSourceHeight);
-        return sourceImageBounds.intersection(tileBoundsInSourceImage);
+        Rectangle normalTileBoundsInSourceImage = new Rectangle(destinationSourceX, destinationSourceY, destinationSourceWidth, destinationSourceHeight);
+        Rectangle normalSourceImageBounds = new Rectangle(this.imageOffset.x, this.imageOffset.y, sourceImageWidth, sourceImageHeight);
+        return normalSourceImageBounds.intersection(normalTileBoundsInSourceImage);
+    }
+
+    protected final void writeDataOnLevelRaster(Raster normalRasterData, Rectangle normalBoundsIntersection, WritableRaster levelDestinationRaster,
+                                                Rectangle levelDestinationRectangle, int bandIndex) {
+
+        int offsetY = this.levelTileOffset.y + levelDestinationRectangle.y;
+        int offsetX = this.levelTileOffset.x + levelDestinationRectangle.x;
+        for (int y = 0; y < levelDestinationRectangle.height; y++) {
+            int currentSrcYOffset = this.imageOffset.y + computeSourceY(offsetY + y);
+            validateCoordinate(currentSrcYOffset, normalBoundsIntersection.y, normalBoundsIntersection.height);
+            for (int x = 0; x < levelDestinationRectangle.width; x++) {
+                int currentSrcXOffset = this.imageOffset.x + computeSourceX(offsetX + x);
+                validateCoordinate(currentSrcXOffset, normalBoundsIntersection.x, normalBoundsIntersection.width);
+                double value = normalRasterData.getSampleDouble(currentSrcXOffset, currentSrcYOffset, bandIndex);
+                levelDestinationRaster.setSample(levelDestinationRectangle.x + x, levelDestinationRectangle.y + y, bandIndex, value);
+            }
+        }
+    }
+
+    protected final int getProductDataType() {
+        int dataBufferType = getSampleModel().getDataType();
+        return ImageManager.getProductDataType(dataBufferType);
+    }
+
+    protected final void writeDataOnLevelRaster(Rectangle normalTileBoundsIntersection, ProductData normalTileData, WritableRaster levelDestinationRaster, Rectangle levelDestinationRectangle) {
+        ProductData destData;
+        boolean directMode = (levelDestinationRaster.getDataBuffer().getSize() == levelDestinationRectangle.width * levelDestinationRectangle.height);
+        if (directMode) {
+            destData = ProductData.createInstance(normalTileData.getType(), ImageUtils.getPrimitiveArray(levelDestinationRaster.getDataBuffer()));
+        } else {
+            destData = ProductData.createInstance(normalTileData.getType(), levelDestinationRectangle.width * levelDestinationRectangle.height);
+        }
+        for (int y = 0; y < levelDestinationRectangle.height; y++) {
+            int currentSrcYOffset = this.imageOffset.y + this.levelImageSupport.getSourceY(this.levelTileOffset.y + levelDestinationRectangle.y + y);
+            int currentDestYOffset = y * levelDestinationRectangle.width;
+            for (int x = 0; x < levelDestinationRectangle.width; x++) {
+                int currentSrcXOffset = this.imageOffset.x + this.levelImageSupport.getSourceX(this.levelTileOffset.x + levelDestinationRectangle.x + x);
+                double value = getSourceValue(normalTileBoundsIntersection, normalTileData, currentSrcXOffset, currentSrcYOffset);
+                destData.setElemDoubleAt(currentDestYOffset + x, value);
+            }
+        }
+        if (!directMode) {
+            levelDestinationRaster.setDataElements(levelDestinationRectangle.x, levelDestinationRectangle.y, levelDestinationRectangle.width, levelDestinationRectangle.height, destData.getElems());
+        }
+    }
+
+    private int computeSourceX(double x) {
+        return this.levelImageSupport.getSourceCoord(x, 0, this.levelImageSupport.getSourceWidth()-1);
+    }
+
+    private int computeSourceY(double y) {
+        return this.levelImageSupport.getSourceCoord(y, 0, this.levelImageSupport.getSourceHeight()-1);
     }
 
     private int getImageWidth() {
@@ -57,5 +111,17 @@ public abstract class AbstractSubsetTileOpImage extends SingleBandedOpImage {
 
     private int getImageHeight() {
         return this.levelImageSupport.getSourceHeight();
+    }
+
+    private static void validateCoordinate(int coordinateToCheck, int minimumCoordinate, int size) {
+        if ((coordinateToCheck < minimumCoordinate) || (coordinateToCheck > (minimumCoordinate + size))) {
+            throw new IllegalStateException("The coordinate " + coordinateToCheck + " is out of bounds. The minimum coordinate is " + minimumCoordinate+ " and the size is " + size + ".");
+        }
+    }
+
+    private static double getSourceValue(Rectangle tileRect, ProductData tileData, int sourceX, int sourceY) {
+        int currentX = sourceX - tileRect.x;
+        int currentY = sourceY - tileRect.y;
+        return tileData.getElemDoubleAt(currentY * tileRect.width + currentX);
     }
 }
--- snap-geotiff/src/main/java/org/esa/snap/dataio/geotiff/GeoTiffTileOpImage.java
+++ snap-geotiff/src/main/java/org/esa/snap/dataio/geotiff/GeoTiffTileOpImage.java
@@ -1,11 +1,8 @@
 package org.esa.snap.dataio.geotiff;
 
 import com.bc.ceres.glevel.MultiLevelModel;
+import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.image.AbstractSubsetTileOpImage;
-import org.esa.snap.core.image.LevelImageSupport;
-import org.esa.snap.core.image.ResolutionLevel;
-import org.esa.snap.core.image.SingleBandedOpImage;
-import org.esa.snap.core.util.ImageUtils;
 
 import javax.media.jai.PlanarImage;
 import java.awt.Dimension;
@@ -22,47 +19,30 @@ public class GeoTiffTileOpImage extends AbstractSubsetTileOpImage {
 
     private final GeoTiffImageReader geoTiffImageReader;
     private final boolean isGlobalShifted180;
+    private final int bandIndex;
 
     public GeoTiffTileOpImage(GeoTiffImageReader geoTiffImageReader, MultiLevelModel imageMultiLevelModel, int dataBufferType, int bandIndex,
                               Rectangle imageBounds, Dimension tileSize, Point tileOffset, int level, boolean isGlobalShifted180) {
 
-        super(imageMultiLevelModel, dataBufferType, bandIndex, imageBounds, tileSize, tileOffset, level);
+        super(imageMultiLevelModel, dataBufferType, imageBounds, tileSize, tileOffset, level);
 
         this.geoTiffImageReader = geoTiffImageReader;
         this.isGlobalShifted180 = isGlobalShifted180;
+        this.bandIndex = bandIndex;
     }
 
     @Override
-    protected void computeRect(PlanarImage[] sources, WritableRaster destinationRaster, Rectangle destinationRectangle) {
-        try {
-            Rectangle tileBoundsIntersection = computeIntersection(destinationRectangle);
-            if (!tileBoundsIntersection.isEmpty()) {
-                Raster rasterData = readRasterData(tileBoundsIntersection.x, tileBoundsIntersection.y, tileBoundsIntersection.width, tileBoundsIntersection.height);
-
-                int offsetY = this.levelTileOffset.y + destinationRectangle.y;
-                int offsetX = this.levelTileOffset.x + destinationRectangle.x;
-                for (int y = 0; y < destinationRectangle.height; y++) {
-                    int currentSrcYOffset = this.imageOffset.y + computeSourceY(offsetY + y);
-                    validateCoordinate(currentSrcYOffset, tileBoundsIntersection.y, tileBoundsIntersection.height);
-                    for (int x = 0; x < destinationRectangle.width; x++) {
-                        int currentSrcXOffset = this.imageOffset.x + computeSourceX(offsetX + x);
-                        validateCoordinate(currentSrcXOffset, tileBoundsIntersection.x, tileBoundsIntersection.width);
-                        double value = rasterData.getSampleDouble(currentSrcXOffset, currentSrcYOffset, this.bandIndex);
-                        destinationRaster.setSample(destinationRectangle.x + x, destinationRectangle.y + y, this.bandIndex, value);
-                    }
-                }
+    protected void computeRect(PlanarImage[] sources, WritableRaster levelDestinationRaster, Rectangle levelDestinationRectangle) {
+        Rectangle normalBoundsIntersection = computeIntersectionOnNormalBounds(levelDestinationRectangle);
+        if (!normalBoundsIntersection.isEmpty()) {
+            Raster normalRasterData;
+            try {
+                normalRasterData = readRasterData(normalBoundsIntersection.x, normalBoundsIntersection.y, normalBoundsIntersection.width, normalBoundsIntersection.height);
+            } catch (IOException ex) {
+                throw new IllegalStateException("Failed to read the data for level " + getLevel() + " and rectangle " + levelDestinationRectangle + ".", ex);
             }
-        } catch (IOException ex) {
-            throw new IllegalStateException("Failed to read the data for level " + getLevel()+" and rectangle " + destinationRectangle + ".", ex);
+            writeDataOnLevelRaster(normalRasterData, normalBoundsIntersection, levelDestinationRaster, levelDestinationRectangle, this.bandIndex);
         }
-    }
-
-    private int computeSourceX(double x) {
-        return this.levelImageSupport.getSourceCoord(x, 0, this.levelImageSupport.getSourceWidth()-1);
-    }
-
-    private int computeSourceY(double y) {
-        return this.levelImageSupport.getSourceCoord(y, 0, this.levelImageSupport.getSourceHeight()-1);
     }
 
     private Raster readRasterData(int destOffsetX, int destOffsetY, int destWidth, int destHeight) throws IOException {
@@ -72,12 +52,6 @@ public class GeoTiffTileOpImage extends AbstractSubsetTileOpImage {
         int sourceOffsetY = sourceStepY * destOffsetY;
         synchronized (this.geoTiffImageReader) {
             return this.geoTiffImageReader.readRect(this.isGlobalShifted180, sourceOffsetX, sourceOffsetY, sourceStepX, sourceStepY, destOffsetX, destOffsetY, destWidth, destHeight);
-        }
-    }
-
-    private static void validateCoordinate(int coordinateToCheck, int minimumCoordinate, int size) {
-        if ((coordinateToCheck < minimumCoordinate) || (coordinateToCheck > (minimumCoordinate + size))) {
-            throw new IllegalStateException("The coordinate " + coordinateToCheck + " is out of bounds. The minimum coordinate is " + minimumCoordinate+ " and the size is " + size + ".");
         }
     }
 }
