279a0bb901888572fef972c99c7cd78b86d553a5
martin-boettcher <martin.boettcher@brockmann-consult.de>
2020-03-24 15:29:43 +0100

    Revert "revert netcdf5 validation (#183)"
    This reverts commit 3dbe44396f80ce10ed7b9537f1ea391cecba3019.
    
    # Conflicts:
    #	snap-geotiff/src/main/java/org/esa/snap/dataio/geotiff/GeoTiffProductReaderPlugIn.java
    #	snap-gpf/src/main/java/org/esa/snap/core/gpf/internal/TileCacheOp.java

--- snap-netcdf/src/test/java/org/esa/snap/dataio/netcdf/nc/MaxStringAttributeLengthTest.java
+++ snap-netcdf/src/test/java/org/esa/snap/dataio/netcdf/nc/MaxStringAttributeLengthTest.java
@@ -28,9 +28,9 @@ public class MaxStringAttributeLengthTest {
     @Before
     public void setUp() throws Exception {
         nc4TempFile = Files.createTempFile(getClass().getSimpleName(), "nc4");
-        nc4Writable = N4FileWriteable.create(nc4TempFile.toString());
+        nc4Writable = NWritableFactory.create(nc4TempFile.toString(),"netcdf4");
         nc3TempFile = Files.createTempFile(getClass().getSimpleName(), "nc3");
-        nc3Writable = N3FileWriteable.create(nc3TempFile.toString());
+        nc3Writable = NWritableFactory.create(nc3TempFile.toString(),"netcdf3");
     }
 
     @After
@@ -61,13 +61,13 @@ public class MaxStringAttributeLengthTest {
 
     @Test
     public void testMaxStringVariableAttributeLengthNC3() throws IOException {
-        final Path tempFile = Files.createTempFile(getClass().getSimpleName(), null);
-        NFileWriteable ncFile = N3FileWriteable.create(tempFile.toString());
+        Path tempFile = Files.createTempFile(getClass().getSimpleName(), null);
+        NFileWriteable ncFile = NWritableFactory.create(tempFile.toString(),"netcdf3");
         try {
             NVariable variable = ncFile.addScalarVariable("metadataVariable", DataType.BYTE);
             variable.addAttribute("longVariableAttributeValue", createLongString(TOO_LONG));
         } finally {
-            if (!tempFile.toFile().delete()) {
+            if (!Files.deleteIfExists(tempFile)) {
                 fail("unable to delete test file");
             }
         }
