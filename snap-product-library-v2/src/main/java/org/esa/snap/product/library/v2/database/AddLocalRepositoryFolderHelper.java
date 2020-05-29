b3b73b9715c65957dc737c3b774473cf6072dabd
jeancoravu <jean.coravu@c-s.ro>
2020-05-29 16:47:30 +0300

    1. Change the expand/collpase icons from the panel representing the product repository. 2. Fix bug when scanning the local repository folders (do not delete from the database when the local product belongs to a child folder.

--- snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/database/AddLocalRepositoryFolderHelper.java
+++ snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/database/AddLocalRepositoryFolderHelper.java
@@ -1,16 +1,7 @@
 package org.esa.snap.product.library.v2.database;
 
-import org.esa.snap.core.dataio.ProductIO;
-import org.esa.snap.core.datamodel.GeoCoding;
-import org.esa.snap.core.datamodel.GeoPos;
-import org.esa.snap.core.datamodel.PixelPos;
-import org.esa.snap.core.datamodel.Product;
-import org.esa.snap.core.datamodel.quicklooks.QuicklookGenerator;
-import org.esa.snap.core.util.ProductUtils;
-import org.esa.snap.remote.products.repository.geometry.Polygon2D;
 import org.esa.snap.remote.products.repository.ThreadStatus;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,29 +18,12 @@ import java.util.stream.Stream;
 /**
  * Created by jcoravu on 4/10/2019.
  */
-public class AddLocalRepositoryFolderHelper {
+public class AddLocalRepositoryFolderHelper extends AbstractLocalRepositoryFolderHelper {
 
     private static final Logger logger = Logger.getLogger(AddLocalRepositoryFolderHelper.class.getName());
 
-    protected final AllLocalFolderProductsRepository allLocalFolderProductsRepository;
-
     public AddLocalRepositoryFolderHelper(AllLocalFolderProductsRepository allLocalFolderProductsRepository) {
-        this.allLocalFolderProductsRepository = allLocalFolderProductsRepository;
-    }
-
-    protected void finishSavingProduct(SaveProductData saveProductData) {
-    }
-
-    protected void invalidProduct(Path path) throws IOException {
-        if (logger.isLoggable(Level.FINE)) {
-            logger.log(Level.FINE, "The path '"+path.toString()+"' does not represent a valid product.");
-        }
-    }
-
-    protected void missingProductGeoCoding(Path path) throws IOException {
-        if (logger.isLoggable(Level.FINE)) {
-            logger.log(Level.FINE, "The local product from the path '"+path.toString()+"' does not contain the geo-coding associated with the scene raster.");
-        }
+        super(allLocalFolderProductsRepository);
     }
 
     public List<SaveProductData> addValidProductsFromFolder(Path localRepositoryFolderPath, ThreadStatus threadStatus)
@@ -73,7 +47,7 @@ public class AddLocalRepositoryFolderHelper {
         return savedProducts;
     }
 
-    protected List<SaveProductData> saveProductsFromFolder(Path localRepositoryFolderPath, List<LocalProductMetadata> existingLocalRepositoryProducts,
+    private List<SaveProductData> saveProductsFromFolder(Path localRepositoryFolderPath, List<LocalProductMetadata> existingLocalRepositoryProducts,
                                                            boolean scanOnlyFirstLevel, ThreadStatus threadStatus)
                                                            throws IOException, InterruptedException {
 
@@ -86,6 +60,7 @@ public class AddLocalRepositoryFolderHelper {
             ThreadStatus.checkCancelled(threadStatus);
 
             if (Files.isDirectory(currentPath)) {
+                // the current path is a folder
                 try (Stream<Path> stream = Files.list(currentPath)) {
 
                     ThreadStatus.checkCancelled(threadStatus);
@@ -138,73 +113,11 @@ public class AddLocalRepositoryFolderHelper {
                     }
                 }
             } else {
+                // the current path is not a folder
                 throw new IllegalStateException("The path '"+currentPath.toString()+"' is not a folder.");
             }
         }
         return savedProducts;
-    }
-
-    private SaveProductData readAndSaveProduct(Path localRepositoryFolderPath, Path productPath, ThreadStatus threadStatus)
-                                               throws IOException, SQLException, InterruptedException {
-
-        SaveProductData saveProductData = null;
-        Product product = ProductIO.readProduct(productPath.toFile());
-        if (product == null) {
-            // the local product has not been read
-            invalidProduct(productPath);
-        } else if (product.getSceneGeoCoding() == null) {
-            // the local product has not geo coding
-            try {
-                product.dispose();
-            } finally {
-                missingProductGeoCoding(productPath);
-            }
-        } else {
-            // the local product has geo coding
-            try {
-                ThreadStatus.checkCancelled(threadStatus);
-
-                Polygon2D polygon2D = buildProductPolygon(product);
-                BufferedImage quickLookImage = null;
-                try {
-                    QuicklookGenerator quicklookGenerator = new QuicklookGenerator();
-                    quickLookImage = quicklookGenerator.createQuickLookFromBrowseProduct(product);
-                } catch (Exception exception) {
-                    logger.log(Level.SEVERE, "Failed to create the quick look image for product '" + product.getName() + "'.", exception);
-                }
-
-                ThreadStatus.checkCancelled(threadStatus);
-
-                saveProductData = this.allLocalFolderProductsRepository.saveLocalProduct(product, quickLookImage, polygon2D, productPath, localRepositoryFolderPath);
-            } finally {
-                product.dispose();
-            }
-        }
-        return saveProductData;
-    }
-
-    private static Polygon2D buildProductPolygon(Product product) {
-        int productWidth = product.getSceneRasterWidth();
-        int productHeight = product.getSceneRasterHeight();
-        PixelPos[] pixelPositions = new PixelPos[5];
-        pixelPositions[0] = new PixelPos(0.0d, 0.0d);
-        pixelPositions[1] = new PixelPos(productWidth, 0.0d);
-        pixelPositions[2] = new PixelPos(productWidth, productHeight);
-        pixelPositions[3] = new PixelPos(0.0d, productHeight);
-        pixelPositions[4] = new PixelPos(0.0d, 0.0d);
-
-        GeoPos[] geographicalPositions = new GeoPos[pixelPositions.length];
-        GeoCoding sceneGeoCoding = product.getSceneGeoCoding();
-        for (int i = 0; i < pixelPositions.length; i++) {
-            geographicalPositions[i] = sceneGeoCoding.getGeoPos(pixelPositions[i], null);
-        }
-        ProductUtils.normalizeGeoPolygon(geographicalPositions);
-
-        Polygon2D polygon2D = new Polygon2D();
-        for (int i = 0; i < geographicalPositions.length; i++) {
-            polygon2D.append(geographicalPositions[i].getLon(), geographicalPositions[i].getLat());
-        }
-        return polygon2D;
     }
 
     private static LocalProductMetadata foundLocalProductMetadata(Path localRepositoryFolderPath, List<LocalProductMetadata> existingLocalRepositoryProducts, Path productPathToCheck) {
