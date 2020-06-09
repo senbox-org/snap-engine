af60856b0892700766c8e9350b0c947ac13c9132
Luis Veci <luis@skywatch.co>
2020-06-09 19:20:31 -0400

    remove use of LongTestRunner

--- snap-dem/src/test/java/org/esa/snap/dem/dataio/TestElevations.java
+++ snap-dem/src/test/java/org/esa/snap/dem/dataio/TestElevations.java
@@ -25,7 +25,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(LongTestRunner.class)
 public class TestElevations {
 
     private static final GeoPos toronto = new GeoPos(43.6532, -79.3832);
