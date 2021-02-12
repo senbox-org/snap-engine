783d4cc36d07b0c3e5d1a3d3f86108cb701d2f84
Alex McVittie <alexander.mcvittie@gmail.com>
2021-02-12 09:03:06 -0500

    Verify there is internet connection before running Copernicus download test

--- snap-dem/src/test/java/org/esa/snap/dem/dataio/copernicus/TestDownload.java
+++ snap-dem/src/test/java/org/esa/snap/dem/dataio/copernicus/TestDownload.java
@@ -1,20 +1,32 @@
 package org.esa.snap.dem.dataio.copernicus;
 
+import org.esa.snap.test.LongTestRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.locationtech.jts.util.Assert;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 
-
+@RunWith(LongTestRunner.class)
 public class TestDownload {
 
 
     @Test
     public void testSearch() throws Exception {
         CopernicusDownloader d = new CopernicusDownloader(Files.createTempDirectory(this.getClass().getSimpleName()).toFile());
-        d.downloadTiles(-80, 43, 30);
-        d.downloadTiles(-80, 43, 90);
-        d.downloadTiles(0, 0, 90);
+        try{
+            URL url = new URL("http://www.google.com");
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            d.downloadTiles(-80, 43, 30);
+            d.downloadTiles(-80, 43, 90);
+        }catch(Exception e){
+            System.out.println("Internet not connected. Download test will be skipped.");
+        }
+
+        //d.downloadTiles(0, 0, 90);
 
     }
     @Test
