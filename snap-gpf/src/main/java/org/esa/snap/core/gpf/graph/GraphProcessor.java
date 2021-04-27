b7ec93888ce8f32a7dc6b132fbcab13647f265a3
Florian1209 <douziechflorian@gmail.com>
2021-04-27 19:14:41 +0200

    add log for gpt test

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/graph/GraphProcessor.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/graph/GraphProcessor.java
@@ -120,6 +120,7 @@ public class GraphProcessor {
             graphContext.dispose();
         } finally {
             pm.done();
+            System.out.println("graph is done");
         }
     }
 
