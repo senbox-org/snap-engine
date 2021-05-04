0cdc94c014a95791acd16b2f62d38ff113446a75
Tom Block <tom.block@brockmann-consult.de>
2021-05-04 08:19:45 +0200

    Reduce memory consumption when loading data for GeoCoding
    * re-organized allocation
    
    * changed call to remove cached tiles
    
    (cherry picked from commit 4a0e6f351e5395b4ce58067ee28830255125e323)

--- snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistable.java
+++ snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistable.java
@@ -1,6 +1,7 @@
 package org.esa.snap.core.dataio.geocoding;
 
 import org.esa.snap.core.dataio.dimap.spi.DimapPersistable;
+import org.esa.snap.core.dataio.geocoding.util.RasterUtils;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.RasterDataNode;
 import org.esa.snap.core.datamodel.TiePointGrid;
@@ -10,6 +11,7 @@ import org.jdom.Element;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+import java.io.IOException;
 import java.awt.Dimension;
 import java.util.stream.IntStream;
 
@@ -156,11 +158,17 @@ public class ComponentGeoCodingPersistable implements DimapPersistable {
         } else {
             final int rasterWidth = lonRaster.getRasterWidth();
             final int rasterHeight = lonRaster.getRasterHeight();
-            final int size = rasterWidth * rasterHeight;
-            final double[] longitudes = lonRaster.getGeophysicalImage().getImage(0).getData()
-                    .getPixels(0, 0, rasterWidth, rasterHeight, new double[size]);
-            final double[] latitudes = latRaster.getGeophysicalImage().getImage(0).getData()
-                    .getPixels(0, 0, rasterWidth, rasterHeight, new double[size]);
+            final double[] longitudes;
+            final double[] latitudes;
+
+            try {
+                longitudes = RasterUtils.loadGeoData(lonRaster);
+                latitudes = RasterUtils.loadGeoData(latRaster);
+            } catch (IOException e) {
+                SystemUtils.LOG.warning("Unable to create " + TAG_COMPONENT_GEO_CODING + ". Reading geo-data failed.");
+                SystemUtils.LOG.severe(e.getMessage());
+                return null;
+            }
             geoRaster = new GeoRaster(longitudes, latitudes, lonVarName, latVarName, rasterWidth, rasterHeight,
                                       resolutionInKm);
         }
