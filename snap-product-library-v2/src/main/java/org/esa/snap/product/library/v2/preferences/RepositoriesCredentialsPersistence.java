3b1c7ef726e297c5f45f93f4bba4a014d3d7473b
Adrian DRAGHICI <adraghici@c-s.ro>
2021-05-21 23:08:55 +0300

    SNAP-1395: Add new feature for query authentication.

--- snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/preferences/RepositoriesCredentialsPersistence.java
+++ snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/preferences/RepositoriesCredentialsPersistence.java
@@ -4,6 +4,7 @@ import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.esa.snap.core.util.StringUtils;
 import org.esa.snap.engine_utilities.util.CryptoUtils;
+import org.esa.snap.product.library.v2.preferences.model.RemoteRepositoryCollectionsCredentials;
 import org.esa.snap.product.library.v2.preferences.model.RemoteRepositoryCredentials;
 import org.esa.snap.product.library.v2.preferences.model.RepositoriesCredentialsConfigurations;
 
@@ -14,7 +15,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -73,11 +76,42 @@ public final class RepositoriesCredentialsPersistence {
      * The preference key for remote repository credential item password.
      */
     private static final String PREFERENCE_KEY_REPOSITORY_CREDENTIAL_SECRET = PREFERENCE_KEY_REPOSITORY_CREDENTIAL + ".password";
+    /**
+     * The pattern for remote repository collection.
+     */
+    private static final String COLL_ID_KEY = "%coll_id%";
+    /**
+     * The regex for extract collections IDs.
+     */
+    private static final String REPOSITORY_COLLECTIONS_IDS_REGEX = PREFERENCE_KEY_REPOSITORY + "\\.collection_(.*?)\\.search_credential\\.username";
+    /**
+     * The preference key for remote repository collection item.
+     */
+    private static final String PREFERENCE_KEY_REPOSITORY_COLLECTION = ".collection_" + COLL_ID_KEY;
+    /**
+     * The preference key for remote repository search credential item.
+     */
+    private static final String PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL = PREFERENCE_KEY_REPOSITORY + PREFERENCE_KEY_REPOSITORY_COLLECTION + ".search_credential";
+    /**
+     * The preference key for remote repository search credential item username.
+     */
+    private static final String PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL_USERNAME = PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL + ".username";
+    /**
+     * The preference key for remote repository search credential item password.
+     */
+    private static final String PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL_SECRET = PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL + ".password";
 
     private static String buildUsernameKey(String repositoryId, String credentialId) {
         String usernameKey = PREFERENCE_KEY_REPOSITORY_CREDENTIAL_USERNAME;
         usernameKey = usernameKey.replace(REPO_ID_KEY, repositoryId);
         usernameKey = usernameKey.replace(CRED_ID_KEY, credentialId);
+        return usernameKey;
+    }
+
+    private static String buildSearchUsernameKey(String repositoryId, String collectionId) {
+        String usernameKey = PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL_USERNAME;
+        usernameKey = usernameKey.replace(REPO_ID_KEY, repositoryId);
+        usernameKey = usernameKey.replace(COLL_ID_KEY, collectionId);
         return usernameKey;
     }
 
@@ -88,8 +122,21 @@ public final class RepositoriesCredentialsPersistence {
         return passwordKey;
     }
 
+    private static String buildSearchPasswordKey(String repositoryId, String collectionId) {
+        String passwordKey = PREFERENCE_KEY_REPOSITORY_COLLECTION_SEARCH_CREDENTIAL_SECRET;
+        passwordKey = passwordKey.replace(REPO_ID_KEY, repositoryId);
+        passwordKey = passwordKey.replace(COLL_ID_KEY, collectionId);
+        return passwordKey;
+    }
+
     private static String buildCredentialsIdsRegex(String repositoryId) {
         String credentialsIdsRegex = REPOSITORY_CREDENTIALS_IDS_REGEX;
+        credentialsIdsRegex = credentialsIdsRegex.replace(REPO_ID_KEY, repositoryId);
+        return credentialsIdsRegex;
+    }
+
+    private static String buildCollectionSearchCredentialsIdsRegex(String repositoryId) {
+        String credentialsIdsRegex = REPOSITORY_COLLECTIONS_IDS_REGEX;
         credentialsIdsRegex = credentialsIdsRegex.replace(REPO_ID_KEY, repositoryId);
         return credentialsIdsRegex;
     }
@@ -98,6 +145,17 @@ public final class RepositoriesCredentialsPersistence {
         for (RemoteRepositoryCredentials repositoryCredentials : repositoriesCredentials) {
             for (Credentials credentials : repositoryCredentials.getCredentialsList()) {
                 if (repositoryCredentials.credentialInvalid(credentials)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean validCollectionsCredentials(List<RemoteRepositoryCollectionsCredentials> repositoriesCollectionsCredentials) {
+        for (RemoteRepositoryCollectionsCredentials repositoryCollectionsCredentials : repositoriesCollectionsCredentials) {
+            for (Credentials credentials : repositoryCollectionsCredentials.getCollectionsCredentials().values()) {
+                if (repositoryCollectionsCredentials.credentialInvalid(credentials)) {
                     return false;
                 }
             }
@@ -140,6 +198,40 @@ public final class RepositoriesCredentialsPersistence {
         }
     }
 
+    private static void saveCollectionsCredentials(Properties properties, List<RemoteRepositoryCollectionsCredentials> repositoriesCollectionsCredentials) {
+        if (validCollectionsCredentials(repositoriesCollectionsCredentials)) {
+            for (RemoteRepositoryCollectionsCredentials repositoryCollectionsCredentials : repositoriesCollectionsCredentials) {
+                String repositoryId = repositoryCollectionsCredentials.getRepositoryName();
+                for (Map.Entry<String, Credentials> collectionCredential : repositoryCollectionsCredentials.getCollectionsCredentials().entrySet()) {
+                    String collectionId = collectionCredential.getKey();
+                    String username = collectionCredential.getValue().getUserPrincipal().getName();
+                    if (StringUtils.isNotNullAndNotEmpty(username)) {
+                        String usernameKey = buildSearchUsernameKey(repositoryId, collectionId);
+                        properties.setProperty(usernameKey, username);
+                    } else {
+                        throw new IllegalArgumentException("empty username");
+                    }
+                    String password = collectionCredential.getValue().getPassword();
+                    if (StringUtils.isNotNullAndNotEmpty(password)) {
+                        String encryptedPassword;
+                        try {
+                            encryptedPassword = CryptoUtils.encrypt(password, repositoryId);
+                        } catch (Exception e) {
+                            throw new IllegalStateException("Failed to encrypt the password.", e);
+                        }
+                        String passwordKey = buildSearchPasswordKey(repositoryId, collectionId);
+                        properties.setProperty(passwordKey, encryptedPassword);
+                    } else {
+                        throw new IllegalArgumentException("empty password");
+                    }
+                }
+            }
+
+        } else {
+            throw new IllegalArgumentException("invalid credentials (empty or duplicates)");
+        }
+    }
+
     private static void saveAutoUncompress(Properties properties, boolean autoUncompress) {
         String autoUncompressVal = "false";
         if (autoUncompress) {
@@ -167,6 +259,7 @@ public final class RepositoriesCredentialsPersistence {
         }
         Properties properties = new Properties();
         saveCredentials(properties, repositoriesCredentialsConfigurations.getRepositoriesCredentials());
+        saveCollectionsCredentials(properties, repositoriesCredentialsConfigurations.getRemoteRepositoryCollectionsCredentials());
         saveAutoUncompress(properties, repositoriesCredentialsConfigurations.isAutoUncompress());
         saveDownloadAllPages(properties, repositoriesCredentialsConfigurations.downloadsAllPages());
         saveRecordsOnPage(properties, repositoriesCredentialsConfigurations.getNrRecordsOnPage());
@@ -221,6 +314,47 @@ public final class RepositoriesCredentialsPersistence {
     }
 
     /**
+     * Reads the Remote Repositories Credentials list from SNAP configuration file.
+     */
+    private static List<RemoteRepositoryCollectionsCredentials> loadCollectionCredentials(Properties properties) {
+        List<RemoteRepositoryCollectionsCredentials> repositoriesCollectionCredentials = new ArrayList<>();
+        Set<String> propertyNames = properties.stringPropertyNames();
+        List<String> repositoriesIdsList = new ArrayList<>();
+        for (String propertyName : propertyNames) {
+            String repositoryId = propertyName.replaceAll(REPOSITORIES_IDS_REGEX, "$1");
+            if (!repositoriesIdsList.contains(repositoryId)) {
+                repositoriesIdsList.add(repositoryId);
+            }
+        }
+        for (String repositoryId : repositoriesIdsList) {
+            Map<String, Credentials> repositoryCollectionCredentials = new LinkedHashMap<>();
+            List<String> collectionsIds = new ArrayList<>();
+            for (String propertyName : propertyNames) {
+                String collectionId = propertyName.replaceAll(buildCollectionSearchCredentialsIdsRegex(repositoryId), "$1");
+                collectionsIds.add(collectionId);
+            }
+            for (String collectionId : collectionsIds) {
+                String usernameKey = buildSearchUsernameKey(repositoryId, collectionId);
+                String username = properties.getProperty(usernameKey);
+                String passwordKey = buildSearchPasswordKey(repositoryId, collectionId);
+                String password = properties.getProperty(passwordKey);
+                try {
+                    password = CryptoUtils.decrypt(password, repositoryId);
+                } catch (Exception e) {
+                    throw new IllegalStateException("Failed to decrypt the password.", e);
+                }
+                if (StringUtils.isNotNullAndNotEmpty(username) && StringUtils.isNotNullAndNotEmpty(password)) {
+                    repositoryCollectionCredentials.put(collectionId, new UsernamePasswordCredentials(username, password));
+                }
+            }
+            if (!repositoryCollectionCredentials.isEmpty()) {
+                repositoriesCollectionCredentials.add(new RemoteRepositoryCollectionsCredentials(repositoryId, repositoryCollectionCredentials));
+            }
+        }
+        return repositoriesCollectionCredentials;
+    }
+
+    /**
      * Reads the Remote Repositories Credentials auto-uncompress setting from SNAP configuration file.
      */
     private static boolean loadAutoUncompress(Properties properties) {
@@ -258,17 +392,18 @@ public final class RepositoriesCredentialsPersistence {
      */
     static RepositoriesCredentialsConfigurations load(Path destFile) throws IOException {
         if (destFile == null || !Files.exists(destFile)) {
-            return new RepositoriesCredentialsConfigurations(new ArrayList<>(), UNCOMPRESSED_DOWNLOADED_PRODUCTS, DOWNLOAD_ALL_PAGES, VISIBLE_PRODUCTS_PER_PAGE);
+            return new RepositoriesCredentialsConfigurations(new ArrayList<>(), new ArrayList<>(), UNCOMPRESSED_DOWNLOADED_PRODUCTS, DOWNLOAD_ALL_PAGES, VISIBLE_PRODUCTS_PER_PAGE);
         }
         Properties properties = new Properties();
         try (InputStream inputStream = Files.newInputStream(destFile)) {
             properties.load(inputStream);
         }
         List<RemoteRepositoryCredentials> repositoriesCredentials = loadCredentials(properties);
+        List<RemoteRepositoryCollectionsCredentials> repositoriesCollectionsCredentials = loadCollectionCredentials(properties);
         boolean autoUncompress = loadAutoUncompress(properties);
         boolean downloadAllPages = loadDownloadAllPages(properties);
         int recordsOnPage = loadRecordsOnPage(properties);
-        return new RepositoriesCredentialsConfigurations(repositoriesCredentials, autoUncompress, downloadAllPages, recordsOnPage);
+        return new RepositoriesCredentialsConfigurations(repositoriesCredentials, repositoriesCollectionsCredentials, autoUncompress, downloadAllPages, recordsOnPage);
     }
 
 
@@ -285,6 +420,17 @@ public final class RepositoriesCredentialsPersistence {
         List<RemoteRepositoryCredentials> itemsToSave = new ArrayList<>();
         itemsToSave.add(repositoryCredentials);
         itemsToSave.add(repositoryCredentials2);
+        Map<String, Credentials> repositoryCollectionsCredentialsMap = new LinkedHashMap<>();
+        repositoryCollectionsCredentialsMap.put("c1", new UsernamePasswordCredentials("u1", "p1"));
+        repositoryCollectionsCredentialsMap.put("c2", new UsernamePasswordCredentials("u2", "p2"));
+        RemoteRepositoryCollectionsCredentials repositoryCollectionsCredentials = new RemoteRepositoryCollectionsCredentials("b1", repositoryCollectionsCredentialsMap);
+        Map<String, Credentials> repositoryCollectionsCredentialsMap2 = new LinkedHashMap<>();
+        repositoryCollectionsCredentialsMap2.put("c1", new UsernamePasswordCredentials("as1", "pw1"));
+        repositoryCollectionsCredentialsMap2.put("c2", new UsernamePasswordCredentials("us2", "pw2"));
+        RemoteRepositoryCollectionsCredentials repositoryCollectionsCredentials2 = new RemoteRepositoryCollectionsCredentials("b2", repositoryCollectionsCredentialsMap2);
+        List<RemoteRepositoryCollectionsCredentials> collectionsItemsToSave = new ArrayList<>();
+        collectionsItemsToSave.add(repositoryCollectionsCredentials);
+        collectionsItemsToSave.add(repositoryCollectionsCredentials2);
 
         boolean autoUncompressToSave = false;
         boolean downloadAllPagesToSave = true;
@@ -292,16 +438,20 @@ public final class RepositoriesCredentialsPersistence {
 
         Path credsFile = Paths.get("D:/Temp/test_pl.properties");
 
-        save(credsFile, new RepositoriesCredentialsConfigurations(itemsToSave, autoUncompressToSave, downloadAllPagesToSave, recordsOnPageToSave));
+        save(credsFile, new RepositoriesCredentialsConfigurations(itemsToSave, collectionsItemsToSave, autoUncompressToSave, downloadAllPagesToSave, recordsOnPageToSave));
 
         RepositoriesCredentialsConfigurations repositoriesCredentialsConfigurations = load(credsFile);
         List<RemoteRepositoryCredentials> itemsLoaded = repositoriesCredentialsConfigurations.getRepositoriesCredentials();
+        List<RemoteRepositoryCollectionsCredentials> collectionsItemsLoaded = repositoriesCredentialsConfigurations.getRemoteRepositoryCollectionsCredentials();
         boolean autoUncompressLoaded = repositoriesCredentialsConfigurations.isAutoUncompress();
         boolean downloadAllPagesLoaded = repositoriesCredentialsConfigurations.downloadsAllPages();
         int recordsOnPageLoaded = repositoriesCredentialsConfigurations.getNrRecordsOnPage();
 
         if (itemsToSave != itemsLoaded) {
             throw new IllegalStateException("items mismatch");
+        }
+        if (collectionsItemsToSave != collectionsItemsLoaded) {
+            throw new IllegalStateException("collection items mismatch");
         }
         if (autoUncompressToSave != autoUncompressLoaded) {
             throw new IllegalStateException("auto uncompress mismatch");
--- snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/preferences/model/RepositoriesCredentialsConfigurations.java
+++ snap-product-library-v2/src/main/java/org/esa/snap/product/library/v2/preferences/model/RepositoriesCredentialsConfigurations.java
@@ -8,12 +8,14 @@ import java.util.List;
 public class RepositoriesCredentialsConfigurations {
 
     private List<RemoteRepositoryCredentials> repositoriesCredentials;
+    private List<RemoteRepositoryCollectionsCredentials> remoteRepositoryCollectionsCredentials;
     private boolean autoUncompress;
     private boolean downloadAllPages;
     private int nrRecordsOnPage;
 
-    public RepositoriesCredentialsConfigurations(List<RemoteRepositoryCredentials> repositoriesCredentials, boolean autoUncompress, boolean downloadAllPages, int nrRecordsOnPage) {
+    public RepositoriesCredentialsConfigurations(List<RemoteRepositoryCredentials> repositoriesCredentials, List<RemoteRepositoryCollectionsCredentials> remoteRepositoryCollectionsCredentials, boolean autoUncompress, boolean downloadAllPages, int nrRecordsOnPage) {
         this.repositoriesCredentials = repositoriesCredentials;
+        this.remoteRepositoryCollectionsCredentials = remoteRepositoryCollectionsCredentials;
         this.autoUncompress = autoUncompress;
         this.downloadAllPages = downloadAllPages;
         this.nrRecordsOnPage = nrRecordsOnPage;
@@ -21,6 +23,10 @@ public class RepositoriesCredentialsConfigurations {
 
     public List<RemoteRepositoryCredentials> getRepositoriesCredentials() {
         return repositoriesCredentials;
+    }
+
+    public List<RemoteRepositoryCollectionsCredentials> getRemoteRepositoryCollectionsCredentials() {
+        return remoteRepositoryCollectionsCredentials;
     }
 
     public boolean isAutoUncompress() {
