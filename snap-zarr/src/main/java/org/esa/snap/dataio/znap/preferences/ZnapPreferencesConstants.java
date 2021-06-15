e1b4bbaa144c67f291573d955e2e14ab0f8d5f4f
sabine <sabine.embacher@brockmann-consult.de>
2021-06-15 08:39:40 +0200

    snap-zarr splitted up to snap-engine and snap-desktop modules

--- snap-zarr/src/main/java/org/esa/snap/dataio/znap/preferences/ZnapPreferencesConstants.java
+++ snap-zarr/src/main/java/org/esa/snap/dataio/znap/preferences/ZnapPreferencesConstants.java
@@ -18,14 +18,16 @@ package org.esa.snap.dataio.znap.preferences;
 
 public final class ZnapPreferencesConstants {
 
-    public static final String PROPERTY_NAME_COMPRESSION_LEVEL = "znap.compression.level";
-    public static final int DEFAULT_COMPRESSION_LEVEL = 3;
-
-    public static final String PROPERTY_NAME_COMPRESSOR_ID = "znap.compressor.id";
-    public static final String DEFAULT_COMPRESSOR_ID = "null";
-
     public static final String PROPERTY_NAME_USE_ZIP_ARCHIVE = "znap.use.zip.archive";
     public static final boolean DEFAULT_USE_ZIP_ARCHIVE = true;
 
     public static final String PROPERTY_NAME_BINARY_FORMAT = "znap.binary.format";
+
+    public static final String PROPERTY_NAME_COMPRESSOR_ID = "znap.compressor.id";
+    public static final String DEFAULT_COMPRESSOR_ID = "null";
+
+    public static final String PROPERTY_NAME_COMPRESSION_LEVEL = "znap.compression.level";
+    public static final int DEFAULT_COMPRESSION_LEVEL = 3;
+
+    public static final Integer[] ZLIB_COMPRESSION_LEVELS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 }
