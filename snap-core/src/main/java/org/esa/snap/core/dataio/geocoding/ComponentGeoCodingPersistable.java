e7ba050a92e5c18cf36eee511a24575f47a2c83c
Marco Peters <marco.peters@brockmann-consult.de>
2021-06-18 14:12:53 +0200

    using snap preferences instead of system properties

--- snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistable.java
+++ snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistable.java
@@ -28,12 +28,14 @@ import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.RasterDataNode;
 import org.esa.snap.core.datamodel.TiePointGrid;
 import org.esa.snap.core.util.SystemUtils;
+import org.esa.snap.runtime.Config;
 import org.geotools.referencing.CRS;
 import org.jdom.Element;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 import java.util.stream.IntStream;
 
 /**
@@ -198,7 +200,8 @@ public class ComponentGeoCodingPersistable implements DimapPersistable {
                                       resolutionInKm);
         }
 
-        final boolean isFractionalEnabled = Boolean.getBoolean(ComponentGeoCoding.SYSPROP_SNAP_PIXEL_CODING_FRACTION_ACCURACY);
+        final Preferences snapPreferences = Config.instance("snap").preferences();
+        final boolean isFractionalEnabled = snapPreferences.getBoolean(ComponentGeoCoding.SYSPROP_SNAP_PIXEL_CODING_FRACTION_ACCURACY, false);
         if (isFractionalEnabled && PixelForward.KEY.equals(forwardKey)) {
             forwardKey = PixelInterpolatingForward.KEY;
         }
--- snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistenceConverter.java
+++ snap-core/src/main/java/org/esa/snap/core/dataio/geocoding/ComponentGeoCodingPersistenceConverter.java
@@ -33,12 +33,14 @@ import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.datamodel.RasterDataNode;
 import org.esa.snap.core.datamodel.TiePointGrid;
 import org.esa.snap.core.util.SystemUtils;
+import org.esa.snap.runtime.Config;
 import org.geotools.referencing.CRS;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.prefs.Preferences;
 import java.util.stream.IntStream;
 
 /**
@@ -230,7 +232,8 @@ public class ComponentGeoCodingPersistenceConverter implements PersistenceConver
                                       resolutionInKm);
         }
 
-        final boolean isFractionalEnabled = Boolean.getBoolean(ComponentGeoCoding.SYSPROP_SNAP_PIXEL_CODING_FRACTION_ACCURACY);
+        final Preferences snapPreferences = Config.instance("snap").preferences();
+        final boolean isFractionalEnabled = snapPreferences.getBoolean(ComponentGeoCoding.SYSPROP_SNAP_PIXEL_CODING_FRACTION_ACCURACY, false);
         if (isFractionalEnabled && PixelForward.KEY.equals(forwardKey)) {
             forwardKey = PixelInterpolatingForward.KEY;
         }
