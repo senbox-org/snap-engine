73a56ec868cded55d9a479d26412d2cc47320c10
Daniel Knowles <daniel.s.knowles@nasa.gov>
2022-06-16 11:54:42 -0400

    Updated performance parameters

--- snap-smart-configurator/src/main/java/org/esa/snap/smart/configurator/VMParameters.java
+++ snap-smart-configurator/src/main/java/org/esa/snap/smart/configurator/VMParameters.java
@@ -92,7 +92,7 @@ public class VMParameters {
 
     public static Path getSnapConfigPath() {
         if(snapConfigPath == null) {
-            snapConfigPath = Config.instance().installDir().resolve("etc").resolve("snap.conf");
+            snapConfigPath = Config.instance().installDir().resolve("etc").resolve("seadas.conf");
         }
         return snapConfigPath;
     }
