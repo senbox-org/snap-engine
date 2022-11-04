ba61e088aace29e483e362ce647c9c3e2b2c6aa3
Daniel Knowles <daniel.s.knowles@nasa.gov>
2022-11-04 10:50:53 -0400

    Collocate: changed an occurance of slave name to dependent name

--- snap-collocation/src/main/java/org/esa/snap/collocation/CollocateOp.java
+++ snap-collocation/src/main/java/org/esa/snap/collocation/CollocateOp.java
@@ -74,9 +74,9 @@ import static java.text.MessageFormat.format;
 public class CollocateOp extends Operator {
 
     public static final String SOURCE_NAME_REFERENCE = "${ORIGINAL_NAME}";
-    public static final String SLAVE_NUMBER_ID_REFERENCE = "${SLAVE_NUMBER_ID}";
+    public static final String SLAVE_NUMBER_ID_REFERENCE = "${DEPENDENT_NUMBER_ID}";
     public static final String DEFAULT_MASTER_COMPONENT_PATTERN = "${ORIGINAL_NAME}_M";
-    public static final String DEFAULT_SLAVE_COMPONENT_PATTERN = "${ORIGINAL_NAME}_S${SLAVE_NUMBER_ID}";
+    public static final String DEFAULT_SLAVE_COMPONENT_PATTERN = "${ORIGINAL_NAME}_D${DEPENDENT_NUMBER_ID}";
     private static final String NEAREST_NEIGHBOUR = "NEAREST_NEIGHBOUR";
     private static final String BILINEAR_INTERPOLATION = "BILINEAR_INTERPOLATION";
     private static final String CUBIC_CONVOLUTION = "CUBIC_CONVOLUTION";
