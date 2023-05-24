a0529deaf0283b2304b5af3c808800eba21b80e3
diana <diana.harosa@c.sro>
2023-05-24 17:40:30 +0300

    Use JUnit rule TemporaryFolder

--- lib-openjpeg/src/test/java/org/esa/snap/lib/openjpeg/dataio/s2/ShortenTest.java
+++ lib-openjpeg/src/test/java/org/esa/snap/lib/openjpeg/dataio/s2/ShortenTest.java
@@ -20,21 +20,24 @@ package org.esa.snap.lib.openjpeg.dataio.s2;
 import org.apache.commons.lang3.SystemUtils;
 import org.esa.snap.core.util.io.FileUtils;
 import org.esa.snap.lib.openjpeg.dataio.Utils;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 
 /**
  * @author opicas-p
-*/
+ */
 public class ShortenTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    private String tempFolderPath ;
 
     @Before
     public void beforeMethod() {
         Assume.assumeTrue(SystemUtils.IS_OS_WINDOWS);
+        tempFolderPath = tempFolder.getRoot().getPath();
     }
 
     /**
@@ -43,12 +46,12 @@ public class ShortenTest {
      */
     @Test
     public void testLongDirectoryName() {
-        String mediumPath = "C:\\5A3AA7c3-475c-42a5-9a25-94d6a93c67b7\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025";
+        String mediumPath = tempFolderPath + "\\5A3AA7c3-475c-42a5-9a25-94d6a93c67b7\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025";
         File directory = new File(mediumPath);
 
         if (directory.mkdirs()) {
             String shortPath = Utils.GetShortPathNameW(mediumPath);
-            Assert.assertEquals("C:\\5A3AA7~1\\S2A_OP~1", shortPath);
+            Assert.assertTrue( shortPath.endsWith("\\5A3AA7~1\\S2A_OP~1"));
             FileUtils.deleteTree(directory.getParentFile());
         }
     }
@@ -59,11 +62,12 @@ public class ShortenTest {
      */
     @Test
     public void testLongFileName() throws Exception {
-        String mediumPath = "C:\\5A3AA7c3-475c-42a5-9a25-94d6a93c67b7\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025.JP2";
+
+        String mediumPath = tempFolderPath + "\\5a3aa7c3-475c-42a5-9a25-94d6a93c67b7\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025.JP2";
         File mediumFile = new File(mediumPath);
         if (mediumFile.getParentFile().mkdir() && mediumFile.createNewFile()) {
             String shortPath = Utils.GetShortPathNameW(mediumPath);
-            Assert.assertEquals("C:\\5A3AA7~1\\S2A_OP~1.JP2", shortPath);
+            Assert.assertTrue( shortPath.endsWith("\\5A3AA7~1\\S2A_OP~1.JP2"));
             FileUtils.deleteTree(mediumFile.getParentFile());
         }
     }
@@ -74,7 +78,7 @@ public class ShortenTest {
      */
     @Test
     public void testVeryLongFileName2() throws Exception {
-        String nonExistingPath = "C:\\5a3aa7c3-475c-42a5-9a25-94d6a93c67b7";
+        String nonExistingPath = tempFolderPath + "\\5a3aa7c3-475c-42a5-9a25-94d6a93c67b7";
         String shortPath = Utils.GetIterativeShortPathNameW(nonExistingPath);
         Assert.assertEquals("", shortPath);
     }
