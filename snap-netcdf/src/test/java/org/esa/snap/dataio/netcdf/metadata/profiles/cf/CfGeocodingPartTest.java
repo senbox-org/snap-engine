40598ab2a779c5ce6368daf29daa1a6e569d58b1
TomBlock <tom.block@brockmann-consult.de>
2024-02-14 14:01:34 +0100

    resolved SNAP-3640

--- snap-netcdf/src/test/java/org/esa/snap/dataio/netcdf/metadata/profiles/cf/CfGeocodingPartTest.java
+++ snap-netcdf/src/test/java/org/esa/snap/dataio/netcdf/metadata/profiles/cf/CfGeocodingPartTest.java
@@ -4,9 +4,12 @@ import com.bc.ceres.annotation.STTM;
 import org.junit.Test;
 import ucar.ma2.Array;
 import ucar.ma2.DataType;
+import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -50,5 +53,87 @@ public class CfGeocodingPartTest {
         verify(variable, times(1)).read();
         verify(array, times(1)).get1DJavaArray(DataType.DOUBLE);
         verifyNoMoreInteractions(variable, array);
+    }
+
+    @Test
+    @STTM("SNAP-3640")
+    public void testGetGeolocationVariables() {
+        final NetcdfFile netcdfFile = mock(NetcdfFile.class);
+
+        final Variable anyVar = mock(Variable.class);
+        when(anyVar.getShortName()).thenReturn("anyVar");
+
+        final Variable lonVar = mock(Variable.class);
+        when(lonVar.getShortName()).thenReturn("longitude");
+
+        final Variable latVar = mock(Variable.class);
+        when(latVar.getShortName()).thenReturn("latitude");
+
+        final List<Variable> variableList = new ArrayList<>();
+        variableList.add(lonVar);
+        variableList.add(anyVar);
+        variableList.add(latVar);
+
+        when(netcdfFile.getVariables()).thenReturn(variableList);
+
+        final CfGeocodingPart.GeoVariables geoVariables = CfGeocodingPart.getGeolocationVariables(netcdfFile, "longitude", "latitude");
+        assertNotNull(geoVariables);
+        assertEquals(lonVar, geoVariables.lonVar);
+        assertEquals(latVar, geoVariables.latVar);
+
+        verify(netcdfFile, times(1)).getVariables();
+        verifyNoMoreInteractions(netcdfFile);
+    }
+
+    @Test
+    @STTM("SNAP-3640")
+    public void testGetGeolocationVariables_lonMissing() {
+        final NetcdfFile netcdfFile = mock(NetcdfFile.class);
+
+        final Variable anyVar = mock(Variable.class);
+        when(anyVar.getShortName()).thenReturn("anyVar");
+
+        final Variable latVar = mock(Variable.class);
+        when(latVar.getShortName()).thenReturn("latitude");
+
+        final List<Variable> variableList = new ArrayList<>();
+        variableList.add(anyVar);
+        variableList.add(latVar);
+
+        when(netcdfFile.getVariables()).thenReturn(variableList);
+
+        final CfGeocodingPart.GeoVariables geoVariables = CfGeocodingPart.getGeolocationVariables(netcdfFile, "longitude", "latitude");
+        assertNotNull(geoVariables);
+        assertNull(geoVariables.lonVar);
+        assertEquals(latVar, geoVariables.latVar);
+
+        verify(netcdfFile, times(1)).getVariables();
+        verifyNoMoreInteractions(netcdfFile);
+    }
+
+    @Test
+    @STTM("SNAP-3640")
+    public void testGetGeolocationVariables_latMissing() {
+        final NetcdfFile netcdfFile = mock(NetcdfFile.class);
+
+        final Variable anyVar = mock(Variable.class);
+        when(anyVar.getShortName()).thenReturn("anyVar");
+
+        final Variable lonVar = mock(Variable.class);
+        when(lonVar.getShortName()).thenReturn("longitude");
+
+        final List<Variable> variableList = new ArrayList<>();
+        variableList.add(lonVar);
+        variableList.add(anyVar);
+
+        when(netcdfFile.getVariables()).thenReturn(variableList);
+
+        final CfGeocodingPart.GeoVariables geoVariables = CfGeocodingPart.getGeolocationVariables(netcdfFile, "longitude", "latitude");
+        assertNotNull(geoVariables);
+        assertEquals(lonVar, geoVariables.lonVar);
+        assertNull(geoVariables.latVar);
+
+        verify(netcdfFile, times(1)).getVariables();
+        verifyNoMoreInteractions(netcdfFile);
     }
 }
