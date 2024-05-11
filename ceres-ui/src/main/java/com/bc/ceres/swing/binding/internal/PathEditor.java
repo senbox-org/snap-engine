26a551c05fa85acd7fc74609dca44f95610a1ce2
Marco <info@eomasters.org>
2024-05-11 12:25:57 +0200

    fixed NPE if default value is null
    added test

--- ceres-ui/src/main/java/com/bc/ceres/swing/binding/internal/PathEditor.java
+++ ceres-ui/src/main/java/com/bc/ceres/swing/binding/internal/PathEditor.java
@@ -9,6 +9,7 @@ import com.bc.ceres.swing.binding.PropertyEditor;
 import javax.swing.*;
 import java.awt.*;
 import java.nio.file.Path;
+import java.util.Optional;
 
 /**
  * An editor for {@link Path}s using a file chooser dialog. It is registered as service in the file
@@ -16,38 +17,41 @@ import java.nio.file.Path;
  */
 public class PathEditor extends PropertyEditor {
 
-  @Override
-  public boolean isValidFor(PropertyDescriptor propertyDescriptor) {
-    return Path.class.isAssignableFrom(propertyDescriptor.getType())
-        && !Boolean.TRUE.equals(propertyDescriptor.getAttribute("directory"));
-  }
+    @Override
+    public boolean isValidFor(PropertyDescriptor propertyDescriptor) {
+        return Path.class.isAssignableFrom(propertyDescriptor.getType())
+                && !Boolean.TRUE.equals(propertyDescriptor.getAttribute("directory"));
+    }
 
-  @Override
-  public JComponent createEditorComponent(PropertyDescriptor propertyDescriptor, BindingContext bindingContext) {
-    final JTextField textField = new JTextField();
-    final ComponentAdapter adapter = new TextComponentAdapter(textField);
-    final Binding binding = bindingContext.bind(propertyDescriptor.getName(), adapter);
-    final JPanel editorPanel = new JPanel(new BorderLayout(2, 2));
-    editorPanel.add(textField, BorderLayout.CENTER);
-    final JButton etcButton = new JButton("...");
-    final Dimension size = new Dimension(26, 16);
-    etcButton.setPreferredSize(size);
-    etcButton.setMinimumSize(size);
-    etcButton.addActionListener(e -> {
-      final JFileChooser fileChooser = new JFileChooser();
-      Path currentFile = (Path) binding.getPropertyValue();
-      if (currentFile != null) {
-        fileChooser.setSelectedFile(currentFile.toFile());
-      } else {
-        Path selectedFile = (Path) propertyDescriptor.getDefaultValue();
-        fileChooser.setSelectedFile(selectedFile.toFile());
-      }
-      int i = fileChooser.showDialog(editorPanel, "Select");
-      if (i == JFileChooser.APPROVE_OPTION && fileChooser.getSelectedFile() != null) {
-        binding.setPropertyValue(fileChooser.getSelectedFile());
-      }
-    });
-    editorPanel.add(etcButton, BorderLayout.EAST);
-    return editorPanel;
-  }
+    @Override
+    public JComponent createEditorComponent(PropertyDescriptor propertyDescriptor, BindingContext bindingContext) {
+        final JTextField textField = new JTextField();
+        final ComponentAdapter adapter = new TextComponentAdapter(textField);
+        final Binding binding = bindingContext.bind(propertyDescriptor.getName(), adapter);
+        final JPanel editorPanel = new JPanel(new BorderLayout(2, 2));
+        editorPanel.add(textField, BorderLayout.CENTER);
+        final JButton etcButton = new JButton("...");
+        final Dimension size = new Dimension(26, 16);
+        etcButton.setPreferredSize(size);
+        etcButton.setMinimumSize(size);
+        etcButton.addActionListener(e -> {
+            final JFileChooser fileChooser = new JFileChooser();
+            Optional<Path> currentPath = getCurrentPath(propertyDescriptor, binding);
+            currentPath.ifPresent(path -> fileChooser.setSelectedFile(path.toFile()));
+            int i = fileChooser.showDialog(editorPanel, "Select");
+            if (i == JFileChooser.APPROVE_OPTION && fileChooser.getSelectedFile() != null) {
+                binding.setPropertyValue(fileChooser.getSelectedFile());
+            }
+        });
+        editorPanel.add(etcButton, BorderLayout.EAST);
+        return editorPanel;
+    }
+
+    static Optional<Path> getCurrentPath(PropertyDescriptor propertyDescriptor, Binding binding) {
+        Path currentFile = (Path) binding.getPropertyValue();
+        if (currentFile == null) {
+            currentFile = (Path) propertyDescriptor.getDefaultValue();
+        }
+        return Optional.ofNullable(currentFile);
+    }
 }
