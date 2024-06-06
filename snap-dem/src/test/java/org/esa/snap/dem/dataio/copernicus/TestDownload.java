945d91387386d5efa7e4be83e4be118714c773d4
Luis Veci <luis@skywatch.co>
2024-06-06 19:30:30 -0400

    SNAP-3717 close product with try() in tests

--- snap-dem/src/test/java/org/esa/snap/dem/dataio/copernicus/TestDownload.java
+++ snap-dem/src/test/java/org/esa/snap/dem/dataio/copernicus/TestDownload.java
@@ -1,10 +1,13 @@
 package org.esa.snap.dem.dataio.copernicus;
 
+import com.bc.ceres.annotation.STTM;
 import com.bc.ceres.test.LongTestRunner;
+import org.esa.snap.core.util.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.locationtech.jts.util.Assert;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
@@ -14,8 +17,10 @@ public class TestDownload {
 
 
     @Test
+    @STTM("SNAP-3717")
     public void testSearch() throws Exception {
-        CopernicusDownloader d = new CopernicusDownloader(Files.createTempDirectory(this.getClass().getSimpleName()).toFile());
+        File folder = Files.createTempDirectory(this.getClass().getSimpleName()).toFile();
+        CopernicusDownloader d = new CopernicusDownloader(folder);
         try{
             URL url = new URL("http://www.google.com");
             URLConnection connection = url.openConnection();
@@ -26,11 +31,10 @@ public class TestDownload {
             System.out.println("Internet not connected. Download test will be skipped.");
         }
 
-        //d.downloadTiles(0, 0, 90);
-
+        FileUtils.deleteTree(folder);
     }
     @Test
-    public void testGetName() throws Exception {
+    public void testGetName() {
         Assert.equals(CopernicusDownloader.createTileFilename(3, -80, "30"), "Copernicus_DSM_COG_30_N03_00_W080_00_DEM");
         Assert.equals(CopernicusDownloader.createTileFilename(-3, -80, "30"), "Copernicus_DSM_COG_30_S03_00_W080_00_DEM");
         Assert.equals(CopernicusDownloader.createTileFilename(3, 80, "30"), "Copernicus_DSM_COG_30_N03_00_E080_00_DEM");
@@ -42,7 +46,5 @@ public class TestDownload {
         Assert.equals(CopernicusDownloader.createTileFilename(3, 80, "10"), "Copernicus_DSM_COG_10_N03_00_E080_00_DEM");
         Assert.equals(CopernicusDownloader.createTileFilename(-3, 80, "10"), "Copernicus_DSM_COG_10_S03_00_E080_00_DEM");
     }
-
-
 
 }
