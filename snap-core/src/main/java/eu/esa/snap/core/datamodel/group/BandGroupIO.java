3c6fed3840a33834381ba9f7aeb7bfd491e7e849
Tom <tom.block@brockmann-consult.de>
2024-06-13 12:28:48 +0200

    first writing steps

--- snap-core/src/main/java/eu/esa/snap/core/datamodel/group/BandGroupIO.java
+++ snap-core/src/main/java/eu/esa/snap/core/datamodel/group/BandGroupIO.java
@@ -1,5 +1,6 @@
 package eu.esa.snap.core.datamodel.group;
 
+import org.esa.snap.core.util.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,10 +9,14 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
+@SuppressWarnings("SuspiciousToArrayCall")
 public class BandGroupIO {
 
+    @SuppressWarnings("unchecked")
     public static BandGrouping[] read(InputStream stream) throws IOException, ParseException {
         final JSONParser jsonParser = new JSONParser();
         final JSONObject jsonObject = (JSONObject) jsonParser.parse(new InputStreamReader(stream, StandardCharsets.UTF_8));
@@ -26,7 +31,7 @@ public class BandGroupIO {
 
             final int numPaths = pathsObject.size();
             final String[][] inputPaths = new String[numPaths][];
-            for (int k = 0; k < numPaths;k++) {
+            for (int k = 0; k < numPaths; k++) {
                 final JSONArray pathList = (JSONArray) pathsObject.get(k);
                 inputPaths[k] = (String[]) pathList.toArray(new String[0]);
             }
@@ -34,11 +39,42 @@ public class BandGroupIO {
             final BandGroupingImpl bandGrouping = new BandGroupingImpl(inputPaths);
 
             final String name = (String) bandGroupObject.get("name");
-            bandGrouping.setName(name);
+            if (StringUtils.isNotNullAndNotEmpty(name)) {
+                bandGrouping.setName(name);
+            }
 
             bandGroupings[i] = bandGrouping;
         }
 
         return bandGroupings;
+    }
+
+    public static void write(BandGrouping[] bandGroupings, OutputStream jsonStream) throws IOException {
+        final JSONObject jsonDoc = new JSONObject();
+        final JSONArray bandGroups = new JSONArray();
+
+        jsonDoc.put("bandGroups", bandGroups);
+
+        for (final BandGrouping grouping : bandGroupings) {
+            final String groupingName = grouping.getName();
+            final JSONObject jsonGrouping = new JSONObject();
+            jsonGrouping.put("name", groupingName);
+
+            final JSONArray pathsArray = new JSONArray();
+            final int numPaths = grouping.size();
+            for (int i = 0; i < numPaths; i++) {
+                final String[] paths = grouping.get(i);
+                JSONArray path = new JSONArray();
+                Collections.addAll(path, paths);
+                pathsArray.add(path);
+            }
+
+            jsonGrouping.put("paths", pathsArray);
+
+            bandGroups.add(jsonGrouping);
+        }
+
+        final String jsonString = jsonDoc.toJSONString();
+        jsonStream.write(jsonString.getBytes(StandardCharsets.UTF_8));
     }
 }
--- snap-core/src/test/java/eu/esa/snap/core/datamodel/group/BandGroupIOTest.java
+++ snap-core/src/test/java/eu/esa/snap/core/datamodel/group/BandGroupIOTest.java
@@ -5,7 +5,9 @@ import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -15,7 +17,7 @@ public class BandGroupIOTest {
     @Test
     @STTM("SNAP-3702")
     public void testRead_noEntries() throws IOException, ParseException {
-        final ByteArrayInputStream emptyStream = new ByteArrayInputStream("{\"bandGroups\" : []}".getBytes());
+        final ByteArrayInputStream emptyStream = new ByteArrayInputStream("{\"bandGroups\" : []}".getBytes(StandardCharsets.UTF_8));
 
         BandGrouping[] bandGroupings = BandGroupIO.read(emptyStream);
         assertEquals(0, bandGroupings.length);
@@ -33,14 +35,146 @@ public class BandGroupIOTest {
                 "  }" +
                 "]" +
                 "}";
-        final ByteArrayInputStream emptyStream = new ByteArrayInputStream(streamData.getBytes());
+        final ByteArrayInputStream jsonStream = new ByteArrayInputStream(streamData.getBytes(StandardCharsets.UTF_8));
 
-        final BandGrouping[] bandGroupings = BandGroupIO.read(emptyStream);
+        final BandGrouping[] bandGroupings = BandGroupIO.read(jsonStream);
         assertEquals(1, bandGroupings.length);
 
         final BandGrouping bandGrouping = bandGroupings[0];
         assertEquals("Jolanda", bandGrouping.getName());
         final String[] strings = bandGrouping.get(0);
         assertArrayEquals(new String[]{"help", "me"}, strings);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testRead_one_noName() throws IOException, ParseException {
+        final String streamData = "{\"bandGroups\" : [" +
+                "  {" +
+                "    \"paths\" : [" +
+                "      [\"nameless\", \"clueless\"]" +
+                "    ]" +
+                "  }" +
+                "]" +
+                "}";
+        final ByteArrayInputStream jsonStream = new ByteArrayInputStream(streamData.getBytes(StandardCharsets.UTF_8));
+
+        final BandGrouping[] bandGroupings = BandGroupIO.read(jsonStream);
+        assertEquals(1, bandGroupings.length);
+
+        final BandGrouping bandGrouping = bandGroupings[0];
+        assertEquals("", bandGrouping.getName());
+        final String[] strings = bandGrouping.get(0);
+        assertArrayEquals(new String[]{"nameless", "clueless"}, strings);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testRead_three() throws IOException, ParseException {
+        final String streamData = "{\"bandGroups\" : [" +
+                "  {" +
+                "    \"name\" : \"Tick\"" +
+                "    \"paths\" : [" +
+                "      [\"the\", \"duck\"]" +
+                "    ]" +
+                "  }," +
+                "  {" +
+                "    \"name\" : \"Trick\"" +
+                "    \"paths\" : [" +
+                "      [\"another\", \"duck\"]," +
+                "      [\"Faehnlein\", \"Fieselschweif\"]" +
+                "    ]" +
+                "  }," +
+                "  {" +
+                "    \"name\" : \"Track\"" +
+                "    \"paths\" : [" +
+                "      [\"young\", \"duck\"]," +
+                "      [\"living\", \"Entenhausen\"]" +
+                "    ]" +
+                "  } " +
+                "]" +
+                "}";
+        final ByteArrayInputStream jsonStream = new ByteArrayInputStream(streamData.getBytes(StandardCharsets.UTF_8));
+
+        final BandGrouping[] bandGroupings = BandGroupIO.read(jsonStream);
+        assertEquals(3, bandGroupings.length);
+
+        BandGrouping bandGrouping = bandGroupings[0];
+        assertEquals("Tick", bandGrouping.getName());
+        String[] strings = bandGrouping.get(0);
+        assertArrayEquals(new String[]{"the", "duck"}, strings);
+
+        bandGrouping = bandGroupings[1];
+        assertEquals("Trick", bandGrouping.getName());
+        strings = bandGrouping.get(1);
+        assertArrayEquals(new String[]{"Faehnlein", "Fieselschweif"}, strings);
+
+        bandGrouping = bandGroupings[2];
+        assertEquals("Track", bandGrouping.getName());
+        strings = bandGrouping.get(0);
+        assertArrayEquals(new String[]{"young", "duck"}, strings);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testWrite_noBandGroups() throws IOException, ParseException {
+        final ByteArrayOutputStream jsonStream = new ByteArrayOutputStream();
+
+        BandGroupIO.write(new BandGrouping[0], jsonStream);
+
+        final String jsonContent = jsonStream.toString();
+        assertEquals("{\"bandGroups\":[]}", jsonContent);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testWrite_one() throws IOException, ParseException {
+        final BandGroupingImpl bandGrouping = new BandGroupingImpl(new String[][]{{"L1B"},
+                {"dim", "rad"}});
+        bandGrouping.setName("Argonaut");
+
+        final ByteArrayOutputStream jsonStream = new ByteArrayOutputStream();
+
+        BandGroupIO.write(new BandGroupingImpl[]{bandGrouping}, jsonStream);
+
+        final String jsonContent = jsonStream.toString();
+        assertEquals("{\"bandGroups\":[{\"paths\":[[\"L1B\"],[\"dim\",\"rad\"]],\"name\":\"Argonaut\"}]}", jsonContent);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testWrite_oneNothingGrouped() throws IOException, ParseException {
+        final BandGroupingImpl bandGrouping = new BandGroupingImpl(new String[0][]);
+        bandGrouping.setName("Empty");
+
+        final ByteArrayOutputStream jsonStream = new ByteArrayOutputStream();
+
+        BandGroupIO.write(new BandGroupingImpl[]{bandGrouping}, jsonStream);
+
+        final String jsonContent = jsonStream.toString();
+        assertEquals("{\"bandGroups\":[{\"paths\":[],\"name\":\"Empty\"}]}", jsonContent);
+    }
+
+    @Test
+    @STTM("SNAP-3702")
+    public void testWrite_three() throws IOException, ParseException {
+        final BandGroupingImpl grouping_1 = new BandGroupingImpl(new String[][]{{"L1B"},
+                {"dim", "rad"}});
+        grouping_1.setName("Anna");
+
+        final BandGroupingImpl grouping_2 = new BandGroupingImpl(new String[][]{{"_err", "_unc"},
+                {"geo_", "in-situ_"}});
+        grouping_2.setName("Bert");
+
+        final BandGroupingImpl grouping_3 = new BandGroupingImpl(new String[][]{{"sensor_sync"}});
+        grouping_3.setName("Christa");
+
+        final ByteArrayOutputStream jsonStream = new ByteArrayOutputStream();
+
+        BandGroupIO.write(new BandGroupingImpl[]{grouping_1, grouping_2, grouping_3}, jsonStream);
+
+        final String jsonContent = jsonStream.toString();
+        assertEquals("{\"bandGroups\":[{\"paths\":[[\"L1B\"],[\"dim\",\"rad\"]],\"name\":\"Anna\"},{\"paths\":[[\"_err\",\"_unc\"],[\"geo_\",\"in-situ_\"]],\"name\":\"Bert\"},{\"paths\":[[\"sensor_sync\"]],\"name\":\"Christa\"}]}",
+                jsonContent);
     }
 }
