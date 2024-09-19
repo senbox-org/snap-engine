884d13e6f807a081e3efce394965fae93e2109b9
dianaharosa <diana.harosa@c-s.ro>
2024-09-19 15:22:51 +0300

    GDAL - remove the gdal.lock file even if the current installation fails

--- lib-gdal/src/main/java/org/esa/snap/dataio/gdal/GDALLoader.java
+++ lib-gdal/src/main/java/org/esa/snap/dataio/gdal/GDALLoader.java
@@ -90,9 +90,10 @@ public final class GDALLoader {
      */
     private void initGDAL() {
         if (isNotInitialised()) {
+            DirectoryLocker gdalDirectoryLocker = null;
             try {
                 this.gdalVersion = GDALVersion.getGDALVersion();
-                DirectoryLocker gdalDirectoryLocker = new DirectoryLocker(this.gdalVersion.getNativeLibrariesFolderPath().getParent());
+                gdalDirectoryLocker = new DirectoryLocker(this.gdalVersion.getNativeLibrariesFolderPath().getParent());
                 while (!gdalDirectoryLocker.tryLockDirectory());
                 GDALDistributionInstaller.setupDistribution(this.gdalVersion);
                 this.gdalVersionLoader = new GDALLoaderClassLoader(new URL[]{this.gdalVersion.getJNILibraryFilePath().toUri().toURL(), GDALVersion.getLoaderLibraryFilePath().toUri().toURL()}, this.gdalVersion.getGDALNativeLibraryFilesPath());
@@ -101,10 +102,13 @@ public final class GDALLoader {
                 initDrivers();
                 GDALDistributionInstaller.setupProj(gdalVersion);
                 postGDALInit();
-                gdalDirectoryLocker.unlockDirectory();
                 logger.log(Level.FINE, () -> "GDAL initialised SUCCESSFULLY!");
             } catch (Throwable t) {
                 logger.log(Level.SEVERE, "Failed to initialize GDAL native drivers. GDAL readers and writers were disabled." + t.getMessage());
+            }finally {
+                if(gdalDirectoryLocker != null) {
+                    gdalDirectoryLocker.unlockDirectory();
+                }
             }
         }
     }
