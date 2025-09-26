54174eea4c965dd2863b218c116dab3ceb18c79e
belu-23 <benjamin.lutz936@gmail.com>
2025-09-26 10:00:03 +0200

    resolves SNAP-4075

--- snap-core/src/main/java/org/esa/snap/core/datamodel/VirtualBandMultiLevelImage.java
+++ snap-core/src/main/java/org/esa/snap/core/datamodel/VirtualBandMultiLevelImage.java
@@ -127,7 +127,9 @@ class VirtualBandMultiLevelImage extends DefaultMultiLevelImage implements Produ
             referencedRasters.add(refRaster);
         }
         for (Product referencedProduct : referencedProducts) {
-            referencedProduct.addProductNodeListener(this);
+            if (referencedProduct != null) {
+                referencedProduct.addProductNodeListener(this);
+            }
         }
     }
 
