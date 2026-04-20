7311e103d4f9d1ff726b95bfb56291dc6e459ed0
Adrian Drăghici <adrian.draghici@cs-soprasteria.com>
2026-04-20 11:20:38 +0300

    [SNAP-1602](snap-gpf): Fix the issue with reading the input product from VFS using ReadOp (make the ReadOp compatible with VFS).

--- snap-gpf/src/main/java/org/esa/snap/core/gpf/common/ReadOp.java
+++ snap-gpf/src/main/java/org/esa/snap/core/gpf/common/ReadOp.java
@@ -40,6 +40,7 @@ import org.esa.snap.core.subset.PixelSubsetRegion;
 import org.esa.snap.core.util.ProductUtils;
 import org.esa.snap.core.util.converters.JtsGeometryConverter;
 import org.esa.snap.core.util.converters.RectangleConverter;
+import org.esa.snap.vfs.NioPaths;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.Polygon;
 
@@ -131,6 +132,7 @@ public class ReadOp extends Operator {
         if (this.file == null) {
             throw new OperatorException("The 'file' parameter is not set");
         }
+        this.file = NioPaths.get(this.file.toString()).toFile();
         if (!this.file.exists()) {
             throw new OperatorException(String.format("Specified 'file' [%s] does not exist.", this.file));
         }
